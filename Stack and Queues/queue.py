bb529c4b30c0ed3e119420468510c29ecc3c164a
anwarvic <mohamedanwarvic@gmail.com>
2018-01-17 22:18:50 +0200

    modified the __repr___() operator for the queue class to be :100:

--- Stack and Queues/queue.py
+++ Stack and Queues/queue.py
@@ -1,46 +1,52 @@
 class Queue():
-	def __init__(self):
-		self.container = []
+    def __init__(self):
+        self.container = []
 
-	def __repr__(self):
-		output = "["
-		for item in self.container:
-			output += str(item)
-			if item != self.container[-1]:
-				output += ", "
-		return output+"]"
+    def __repr__(self):
+        top_border = '' if self.container else '┬'
+        middle = '' if self.container else '│'
+        down_border = '' if self.container else '┴'
+        for item in self.container:
+            width = len(str(item))+2 #2: for a space before & after item
+            top_border += ('─'*width) + '┬'
+            middle += " {} │".format(item)
+            down_border += ('─'*width) + '┴'
+        # add extension
+        top_border += '─'
+        down_border += '─'
+        return "{}\n{}\n{}".format(top_border, middle, down_border)
 
-	def __len__(self):
-		return len(self.container)
+    def __len__(self):
+        return len(self.container)
 
-	def enqueue(self, item):
-		self.container.append(item)
+    def enqueue(self, item):
+        self.container.append(item)
 
-	def dequeue(self):
-		return self.container.pop(0)
+    def dequeue(self):
+        return self.container.pop(0)
 
-	def get_head(self):
-		return self.container[0]
+    def get_head(self):
+        return self.container[0]
 
-	def get_tail(self):
-		return self.container[-1]
+    def get_tail(self):
+        return self.container[-1]
 
-	def is_empty(self):
-		return self.container == 0
+    def is_empty(self):
+        return self.container == 0
 
-	def clear(self):
-		self.container = []
+    def clear(self):
+        self.container = []
 
 
 
 if __name__ == "__main__":
-	q = Queue()
-	q.enqueue(0)
-	q.enqueue(1)
-	q.enqueue(8)
-	q.enqueue(9)
-	print(q.get_head())
-	q.dequeue()
-	print(q)
-	q.clear()
-	print(q)
+    q = Queue()
+    q.enqueue(0)
+    q.enqueue(1)
+    q.enqueue(8)
+    q.enqueue(9)
+    print(q.get_head())
+    q.dequeue()
+    print(q)
+    q.clear()
+    print(q)
