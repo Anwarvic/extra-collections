0b71b881d1f9203c4f6eafa84221822cb7bcb53b
anwarvic <mohamedanwarvic@gmail.com>
2018-01-18 22:28:05 +0200

    adding documentation to the queue script

--- Stack and Queues/queue.py
+++ Stack and Queues/queue.py
@@ -1,11 +1,12 @@
 class Queue():
+    """Basic object for the Queue data structure"""
     def __init__(self):
         self.container = []
 
     def __repr__(self):
-        top_border = '' if self.container else '┬'
-        middle = '' if self.container else '│'
-        down_border = '' if self.container else '┴'
+        top_border = '─┬'
+        middle = ' │'
+        down_border = '─┴'
         for item in self.container:
             width = len(str(item))+2 #2: for a space before & after item
             top_border += ('─'*width) + '┬'
@@ -13,28 +14,35 @@ class Queue():
             down_border += ('─'*width) + '┴'
         # add extension
         top_border += '─'
+        middle += ' '
         down_border += '─'
         return "{}\n{}\n{}".format(top_border, middle, down_border)
 
     def __len__(self):
-        return len(self.container)
+        return len(self.container) #TODO remove container
 
     def enqueue(self, item):
+        """Insert value into the Queue"""
         self.container.append(item)
 
     def dequeue(self):
+        """Removes value from the Queue (Queue's head)"""
         return self.container.pop(0)
 
     def get_head(self):
+        """Returns the Qeueu head (first element to be enqueued) """
         return self.container[0]
 
     def get_tail(self):
+        """Returns the Qeueu tail (last element to be enqueued) """
         return self.container[-1]
 
     def is_empty(self):
+        """Checks if the Queue is empty"""
         return self.container == 0
 
     def clear(self):
+        """Clears the whole Queue"""
         self.container = []
 
 
@@ -42,10 +50,12 @@ class Queue():
 if __name__ == "__main__":
     q = Queue()
     q.enqueue(0)
+    print(q)
     q.enqueue(1)
     q.enqueue(8)
     q.enqueue(9)
     print(q.get_head())
+    print(q)
     q.dequeue()
     print(q)
     q.clear()
