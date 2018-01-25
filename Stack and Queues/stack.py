09d348146f0ca59d907bd328b524b124068fd92a
anwarvic <mohamedanwarvic@gmail.com>
2018-01-25 21:36:14 +0200

    added max_capacity argument to the stack class

--- Stack and Queues/stack.py
+++ Stack and Queues/stack.py
@@ -1,7 +1,9 @@
 class Stack():
     """Basic object for the Stack data structure"""
-    def __init__(self):
+    def __init__(self, max_capacity=None):
         self.container = []
+        self.max_capacity = max_capacity
+
 
     def __repr__(self):
         top_border = '┌'
@@ -18,24 +20,31 @@ class Stack():
         down_border += '─'
         return "{}\n{}\n{}".format(top_border, middle, down_border)
 
+
     def __len__(self):
         return len(self.container)
 
+
     def push(self, item):
         """Pushs item to the stack"""
-        self.container.append(item)
+        if self.max_capacity and len(self) < self.max_capacity:
+            self.container.append(item)
+
 
     def pop(self):
         """Pops item from the stack"""
         return self.container.pop()
 
+
     def peek(self):
         """Returns the top item from the stack"""
         return self.container[-1]
 
+
     def is_empty(self):
         """Checks if the stack is empty"""
         return len(self) == 0
+
 
     def clear(self):
         """Clears the stack"""
@@ -44,12 +53,13 @@ class Stack():
 
 
 if __name__ == "__main__":
-    s = Stack()
+    s = Stack(max_capacity=3)
     s.push(2) #2
     print(s)
     s.push(40) #2 40
     print(s)
     s.push(800) #2 40 800
+    s.push(1000000) #2 40 800 (won't push)
     print(s)
 
     print('='*20)
