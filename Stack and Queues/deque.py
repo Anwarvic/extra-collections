414df67ad6249e7e722f9bb58529fefe103ad97e
anwarvic <mohamedanwarvic@gmail.com>
2018-01-26 21:50:42 +0200

    adding documentation for a few methods in deque

--- Stack and Queues/deque.py
+++ Stack and Queues/deque.py
@@ -4,7 +4,9 @@ class Deque():
         self.container = []
         self.max_capacity = max_capacity
 
+
     def __repr__(self):
+        """Represents the deque as a string."""
         top_border = '─┬'
         middle = ' │'
         down_border = '─┴'
@@ -19,8 +21,11 @@ class Deque():
         down_border += '─'
         return "{}\n{}\n{}".format(top_border, middle, down_border)
 
+
     def __len__(self):
+        """Gets the length of the deque with complexity of O(1)"""
         return len(self.container)
+
 
     def add_left(self, item):
         """Insert value into the front of the Deque"""
@@ -28,27 +33,33 @@ class Deque():
             self.remove_right()
         self.container.insert(0, item)
 
+
     def add_right(self, item):
         """Insert value into the end of the Deque"""
         if self.max_capacity and len(self) == self.max_capacity:
             self.remove_left()
         self.container.append(item)
 
+
     def remove_left(self):
         """Removes value from the Deque (first-inserted item)"""
         return self.container.pop(0)
+
 
     def remove_right(self):
         """Removes value from the Deque (most recent)"""
         return self.container.pop()
 
+
     def peek(self):
         """Returns the Deque recent item"""
         return self.container[-1]
 
+
     def is_empty(self):
         """Checks if the Deque is empty"""
         return len(self) == 0
+
 
     def clear(self):
         """Clears the whole Deque"""
