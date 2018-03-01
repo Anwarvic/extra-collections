88123889a040c226d0680c590587227dfc998b04
anwarvic <mohamedanwarvic@gmail.com>
2018-03-01 17:53:57 +0200

    now, we can initialize bst using a list of numbers

--- Trees/bst.py
+++ Trees/bst.py
@@ -6,16 +6,41 @@ class TreeNode():
         self.right = None
 
     def __repr__(self):
-        return str(self.data)
+        return str(self.number)
 
 
 
 class BST:
     def __init__(self, value):
-        self.root = TreeNode(value)
+        if type(value) == list:
+            lst = sorted(value)
+            self.root = self.__init_bst(lst)
+        else:
+            self.root = TreeNode(value)
+
+    def __init_bst(self, lst):
+        length = len(lst)
+        if length == 1:
+            parent = TreeNode(lst[0])
+        elif length == 2:
+            parent = TreeNode(lst[0])
+            parent.left = TreeNode(lst[1])
+        else:
+            mid_idx = len(lst)//2
+            parent = TreeNode(lst[mid_idx])
+            parent.left = self.__init_bst(lst[0:mid_idx])
+            parent.right = self.__init_bst(lst[mid_idx+1:])
+        return parent
+
 
 
 
 
 if __name__ == "__main__":
+    # initialize tree by numbers
     tree = BST(4)
+
+    # initialize tree by list
+    lst = [7,10,12,22,30,11,19,25,9,20,14]
+    btree = BST(lst)
+    print(btree.root.right.right.left)
