8e5e004f779f84a5e9533789774f5b0e99237775
anwarvic <mohamedanwarvic@gmail.com>
2018-04-12 09:01:39 +0200

    removed unnecessary private methods

--- Trees/avl.py
+++ Trees/avl.py
@@ -22,30 +22,6 @@ class AVL(BST):
     def __init__(self, value):
         self.root = TreeNode(value)
 
-    def __get_depth(self, start_node):
-        depth = 0
-        if start_node != None:
-            left_depth, right_depth = 0, 0
-            if start_node.left:
-                left_depth = 1 + self.__get_depth(start_node.left)
-            if start_node.right:
-                right_depth = 1 + self.__get_depth(start_node.right)
-            depth += max(left_depth, right_depth)
-        return depth
-
-    def __is_balanced(self, start_node):
-        """
-        Tree is said to be balanced if the difference between the depth of any
-        two leaf nodes is one or less.
-        """
-        # check left subtree
-        left_depth = 1 if start_node.left != None else 0
-        left_depth += self.__get_depth(start_node.left)
-        # check right subtree
-        right_depth = 1 if start_node.right != None else 0
-        right_depth += self.__get_depth(start_node.right)
-        return abs(left_depth - right_depth) <= 1
-
     def rotate_left(self, start_node):
         middle = start_node.right
         middle.parent = start_node.parent
@@ -113,7 +89,7 @@ if __name__ == "__main__":
     print(avl)
     print('='*50)
     
-    # to test righ-left rotation
+    # to test right-left rotation
     avl = AVL(1)
     avl.root.set_right(TreeNode(2))
     avl.root.right.set_left(TreeNode(3))
