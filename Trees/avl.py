69829c9450b3deeec9d9926a7084c82d63a4b916
anwarvic <mohamedanwarvic@gmail.com>
2018-04-12 08:22:22 +0200

    rotate_left_right() method is finished in AVL

--- Trees/avl.py
+++ Trees/avl.py
@@ -49,33 +49,41 @@ class AVL(BST):
     def rotate_left(self, start_node):
         middle = start_node.right
         middle.parent = start_node.parent
-        middle.set_left(start_node)
         start_node.right = None
+        middle.set_left(start_node)
         return middle
 
     def rotate_right(self, start_node):
         middle = start_node.left
         middle.parent = start_node.parent
-        middle.set_right(start_node)
         start_node.left = None
+        middle.set_right(start_node)
+        return middle
+
+    def rotate_left_right(self, start_node):
+        middle = start_node.left.right
+        middle.parent = start_node.parent
+        start_node.left.right = None
+        middle.set_left(start_node.left)
+        start_node.left = None
+        middle.set_right(start_node)
         return middle
 
 
 
 
-
-
 if __name__ == "__main__":
-    # # to test left rotation
-    # avl = AVL(1)
-    # avl.root.set_right(TreeNode(2))
-    # avl.root.right.set_right(TreeNode(3))
-    # avl.root.right.right.set_right(TreeNode(4))
-    # avl.root.right.right.right.set_right(TreeNode(5))
-    # print(avl)
-    # avl.root = avl.rotate_left(avl.root)
-    # avl.root.right = avl.rotate_left(avl.root.right)
-    # print(avl)
+    # to test left rotation
+    avl = AVL(1)
+    avl.root.set_right(TreeNode(2))
+    avl.root.right.set_right(TreeNode(3))
+    avl.root.right.right.set_right(TreeNode(4))
+    avl.root.right.right.right.set_right(TreeNode(5))
+    print(avl, '\n')
+    avl.root = avl.rotate_left(avl.root)
+    avl.root.right = avl.rotate_left(avl.root.right)
+    print(avl)
+    print('='*50)
 
     # to test right rotation
     avl = AVL(1)
@@ -83,9 +91,18 @@ if __name__ == "__main__":
     avl.root.left.set_left(TreeNode(3))
     avl.root.left.left.set_left(TreeNode(4))
     avl.root.left.left.left.set_left(TreeNode(5))
-    print(avl)
+    print(avl, '\n')
     avl.root = avl.rotate_right(avl.root)
     avl.root.left = avl.rotate_right(avl.root.left)
     print(avl)
+    print('='*50)
 
+    # to test right rotation
+    avl = AVL(1)
+    avl.root.set_left(TreeNode(2))
+    avl.root.left.set_right(TreeNode(3))
+    print(avl, '\n')
+    avl.root = avl.rotate_left_right(avl.root)
+    print(avl)
+    print('='*50)
     
