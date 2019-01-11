b7bd7ec36fc47317648db7c5ac066a7d020fd5ec
anwarvic <mohamedanwarvic@gmail.com>
2019-01-11 13:11:06 +0200

    refactored the whole heap class.. now Heap is an abstract class and MinHeap and MaxHeap will be created later

--- Trees/heap.py
+++ Trees/heap.py
@@ -1,59 +1,28 @@
-from avl import TreeNode
-from bst import BST
+from abc import ABC
+
+from binary_tree import TreeNode, BinaryTree
 
 
-class MinHeap(BST):
+class Heap():
     def __init__(self, value):
-        if type(value) == list:
-            self.length = len(value)
-            self.root = self.__heapify(value)
-        else:
-            self.length = 1
-            self.root = TreeNode(value)
+        assert type(value) in {int, float}, "Heap contains numbers only!!"
+        self.heap = [value]
 
-    def __heapify(self, lst):
-        lst = sorted(lst)
+    @abstractmethod
+    def heapify(lst):
         length = len(lst)
-        assert length >0, "Given List must have values!!"
         if length == 1:
             node = TreeNode(lst[0])
         elif length == 2:
             node = TreeNode(lst[0])
-            node.set_left( TreeNode(lst[1]) )
+            node.left = TreeNode(lst[1])
         else:
             node = TreeNode(lst[0])
-            node.set_left( self.__heapify(lst[1::2]) )
-            node.set_right( self.__heapify(lst[2::2]) )
+            node.left = self.__heapify(lst[1::2])
+            node.right = self.__heapify(lst[2::2])
+        btree = BinaryTree(0)
+        btree.root = node
         return node
 
 
-    def __len__(self):
-        return self.length
 
-    def get_min(self):
-        return self.root.data
-
-    def get_max(self):
-        raise NotImplementedError("This is a min-heap!!")
-
-
-    ############################## INSERTION ##############################
-    def swap(self, parent, start_node):
-        pass
-
-
-    def insert(self, value):
-        assert type(value) in {int, float}, "You can insert only numbers!"
-        self.length += 1
-        
-
-
-
-
-
-if __name__ == "__main__":
-    h = MinHeap([4,3,1,2, 5, 80, 10, 20, 30, 40, 52, 10])
-    print(h)
-    print(h.get_min())
-    print("Heap Traverse:", h.traverse("depth-first"))
-    print(len(h))
