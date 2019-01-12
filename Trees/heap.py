10d7a19d5575ab99b61e941713105167614e274b
anwarvic <mohamedanwarvic@gmail.com>
2019-01-12 15:06:21 +0200

    refactored MinHeap a little bit... Heap is now an abstract class

--- Trees/heap.py
+++ Trees/heap.py
@@ -5,6 +5,9 @@ from binary_tree import TreeNode, BinaryTree
 
 class Heap(ABC):
     @abstractmethod
+    def __init__(self):
+        pass
+
     def heapify(lst):
         length = len(lst)
         if length == 1:
@@ -18,12 +21,28 @@ class Heap(ABC):
             node.right = Heap.heapify(lst[2::2])
         return node
 
+    def insert(self, value, swap_needed):
+        # add the new value
+        self.heap.append(value)
+        # swap between parents when needed
+        idx = len(self.heap)-1
+        while(idx != 0):
+            parent_idx = (idx-1)//2
+            current = self.heap[idx]
+            parent = self.heap[parent_idx]
+            if swap_needed(parent, current):
+                self.heap[parent_idx], self.heap[idx] = \
+                                        self.heap[idx], self.heap[parent_idx]
+                idx = parent_idx
+            else:
+                break
 
 
 
 
 
-class MinHeap():
+
+class MinHeap(Heap):
 
     def __init__(self, value):
         if type(value) in {list, set, frozenset}:
@@ -39,23 +58,16 @@ class MinHeap():
 
     def __repr__(self):
         root = Heap.heapify(self.heap)
-        return str( BinaryTree(root) )
+        btree = BinaryTree(root)
+        return str( btree )
+
+
+    ############################## INSERTION ##############################
+    def __is_parent_bigger(self, parent, child):
+        return parent > child
 
     def insert(self, value):
-        # add the new value
-        self.heap.append(value)
-        # swap between parents when needed
-        idx = len(self.heap)-1
-        while(idx != 0):
-            parent_idx = (idx-1)//2
-            current = self.heap[idx]
-            parent = self.heap[parent_idx]
-            if parent > current:
-                self.heap[parent_idx], self.heap[idx] = \
-                                        self.heap[idx], self.heap[parent_idx]
-                idx = parent_idx
-            else:
-                break
+        super().insert(value, self.__is_parent_bigger)
 
     def get_min(self):
         return self.heap[0]
@@ -67,7 +79,10 @@ class MinHeap():
 
 
 
+
+
 if __name__ == "__main__":
+    # h = Heap()
     heap = MinHeap([1,2,3,4])
     heap.insert(1)
     heap.insert(2)
