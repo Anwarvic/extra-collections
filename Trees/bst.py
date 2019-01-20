fb5211b19920f9dbeac6367bdc0e2349c55baa20
anwarvic <mohamedanwarvic@gmail.com>
2019-01-20 22:47:05 +0200

    BST treenodes has parent member variable now which could reduce repeated code with AVL and SplayTree classes

--- Trees/bst.py
+++ Trees/bst.py
@@ -4,34 +4,47 @@ from binary_tree import TreeNode, BinaryTree
 class TreeNode(TreeNode):
     def __init__(self, value):
         assert type(value) in {int, float}, "BST contains only numbers!!"
-        self.data = value
-        self.left = None
-        self.right = None
+        super().__init__(value)
+        self.parent = None
+
+    def set_left(self, new_node):
+        self.left = new_node
+        if new_node is not None:
+            self.left.parent = self
+
+
+    def set_right(self, new_node):
+        self.right = new_node
+        if new_node is not None:
+            self.right.parent = self
 
 
 class BST(BinaryTree):
     def __init__(self, value):
         if isinstance(value, TreeNode):
             self.root = value
-        if hasattr(value, '__iter__'):
+        elif type(value) in {set, frozenset}:
             lst = sorted(value)
+            self.root = self.__init_bst(lst)
+        elif hasattr(value, '__iter__'):
+            lst = sorted(set(value))
             self.root = self.__init_bst(lst)
         else:
             self.root = TreeNode(value)
 
     def __init_bst(self, lst):
         length = len(lst)
-        assert length >0, "Given List must have values!!"
+        assert length >0, "Given list must have t lease on item!!"
         if length == 1:
             node = TreeNode(lst[0])
         elif length == 2:
             node = TreeNode(lst[0])
-            node.left = TreeNode(lst[1])
+            node.set_left( TreeNode(lst[1]) )
         else:
             mid_idx = len(lst)//2
             node = TreeNode(lst[mid_idx])
-            node.left = self.__init_bst(lst[0:mid_idx])
-            node.right = self.__init_bst(lst[mid_idx+1:])
+            node.set_left( self.__init_bst(lst[0:mid_idx]) )
+            node.set_right( self.__init_bst(lst[mid_idx+1:]) )
         return node
 
 
@@ -87,12 +100,12 @@ class BST(BinaryTree):
             if start_node.left:
                 self.__insert(value, start_node.left)
             else:
-                start_node.left = TreeNode(value)
+                start_node.set_left( TreeNode(value) )
         else:
             if start_node.right:
                 self.__insert(value, start_node.right)
             else:
-                start_node.right = TreeNode(value)
+                start_node.set_right( TreeNode(value) )
 
     def insert(self, value):
         assert type(value) in {int, float}, "You can insert only numbers!"
@@ -104,19 +117,19 @@ class BST(BinaryTree):
         if del_value == start_node.data:
             if start_node.is_leaf():
                 if del_value < parent.data:
-                    parent.left = None
+                    parent.set_left( None )
                 else:
-                    parent.right = None
+                    parent.set_right( None )
             elif start_node.left != None and start_node.right == None:
                 if del_value < parent.data:
-                    parent.left = start_node.left
+                    parent.set_left( start_node.left )
                 else:
-                    parent.right = start_node.left
+                    parent.set_right( start_node.left )
             elif start_node.left == None and start_node.right != None:
                 if del_value < parent.data:
-                    parent.left = start_node.right
+                    parent.set_left( start_node.right )
                 else:
-                    parent.right = start_node.right
+                    parent.set_right( start_node.right )
             else:
                 replacement_node = self.__get_max_node(start_node.left)
                 start_node.data = replacement_node.data
@@ -161,27 +174,27 @@ if __name__ == "__main__":
     print('='*50)
     #######################################
     # initialize tree by list
-    lst = [7,10,12,22,30,11,19,25,9,20,14]
+    lst = [7,10,12,22,30,11,19,25,9,20,14,12]
     bst = BST(lst)
     print(bst)
     print('='*50)
     #######################################
     # example taken from "Data Structures and Algorithms in Python" book
     bst = BST(44)
-    bst.root.left = TreeNode(17)
-    bst.root.left.left = TreeNode(8)
-    bst.root.left.right = TreeNode(32)
-    bst.root.left.right.left = TreeNode(28)
-    bst.root.left.right.left.right = TreeNode(29)
-    bst.root.right = TreeNode(88)
-    bst.root.right.right = TreeNode(97)
-    bst.root.right.right.left = TreeNode(93)
-    bst.root.right.left = TreeNode(65)
-    bst.root.right.left.left = TreeNode(54)
-    bst.root.right.left.right = TreeNode(82)
-    bst.root.right.left.right.left = TreeNode(76)
-    bst.root.right.left.right.left.left = TreeNode(68)
-    bst.root.right.left.right.left.right = TreeNode(80)
+    bst.root.set_left(TreeNode(17))
+    bst.root.left.set_left(TreeNode(8))
+    bst.root.left.set_right( TreeNode(32) )
+    bst.root.left.right.set_left( TreeNode(28) )
+    bst.root.left.right.left.set_right( TreeNode(29) )
+    bst.root.set_right( TreeNode(88) )
+    bst.root.right.set_right( TreeNode(97) )
+    bst.root.right.right.set_left( TreeNode(93) )
+    bst.root.right.set_left( TreeNode(65) )
+    bst.root.right.left.set_left( TreeNode(54) )
+    bst.root.right.left.set_right( TreeNode(82) )
+    bst.root.right.left.right.set_left( TreeNode(76) )
+    bst.root.right.left.right.left.set_left( TreeNode(68) )
+    bst.root.right.left.right.left.set_right( TreeNode(80) )
     print(bst)
 
     bst.remove(32)
