45336cdb3148333558571cc7c6642889f9638faa
anwarvic <mohamedanwarvic@gmail.com>
2019-01-21 23:59:34 +0200

    added zig-zig operation in SplayTree class

--- Trees/splayTree.py
+++ Trees/splayTree.py
@@ -1,12 +1,36 @@
-from bst import BST
-from avl import TreeNode
+from bst import TreeNode, BST
 
 class SplayTree(BST):
-    pass
+    def zig_zig(self, start_node, left_children=True):
+        child = start_node
+        parent = start_node.parent
+        grand_parent = parent.parent
+        # start zig-zig
+        if left_children:
+            print("Left zig-zig")
+            child.parent = grand_parent.parent
+            grand_parent.set_left(parent.right)
+            parent.set_right(grand_parent)
+            parent.set_left(child.right)
+            child.set_right(parent)
+        else:
+            print("Right zig-zig")
+            child.parent = grand_parent.parent
+            grand_parent.set_right(parent.left)
+            parent.set_left(grand_parent)
+            parent.set_right(child.left)
+            child.set_left(parent)
+        #child is now the grand-parent
+        return child
+
+
 
 
 
 
 if __name__ == "__main__":
-    stree = SplayTree(1)
+    stree = SplayTree(10)
+    stree.insert(20)
+    stree.insert(30)
+    stree.root = stree.zig_zig(stree.root.right.right, left_children=False)
     print(stree)
