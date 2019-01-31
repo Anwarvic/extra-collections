097837acba9459d2b5e66712eac0ccc12eabfc45
anwarvic <mohamedanwarvic@gmail.com>
2019-01-31 11:06:06 +0200

    adding a few notes to red-black trees

--- Trees/red_black_tree.py
+++ Trees/red_black_tree.py
@@ -1,3 +1,21 @@
+"""
+Red-Black Trees are self-balanced BSTs (Binary Search Trees) which means that
+they have a guaranteed hieght of O(log(n)) where n is the number of nodes within
+the tree. The color in red-black trees is used only to re-balance trees when 
+they get imbalanced.
+
+Red black trees have the following characteristics:
+- A node is either 'red' or 'black'
+- Tree root is always black
+- A node is 'red' if it has two black children.
+- Any child of a red-node is always black
+
+Some other properties:
+- "Black depth": is the depth of all black nodes.
+- "Shortest path": (from parent to leaf-node) is equal to the black depth.
+- "Longest path": is the one with alternating red and black nodes.
+- The longest path can't be bigger than (2*shorted-path).
+"""
 from enum import Enum
 from bst import TreeNode, BST
 
@@ -18,6 +36,7 @@ class RedBlackTree(BST):
     def __init__(self, value):
         self.root = TreeNode(value)
         self.root.color = Color.BLACK
+
 
     def insert(self, value):
         pass
