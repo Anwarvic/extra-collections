b1d96efd363d8582b43f0a8b7f3d0f40cdacf1b2
anwarvic <mohamedanwarvic@gmail.com>
2019-01-31 10:24:27 +0200

    created Enum for colors in Red-black trees

--- Trees/red_black_tree.py
+++ Trees/red_black_tree.py
@@ -1,16 +1,28 @@
+from enum import Enum
 from bst import TreeNode, BST
+
+
+class Color(Enum):
+    BLACK = 0
+    RED = 1
 
 
 class TreeNode(TreeNode):
     def __init__(self, value):
         super().__init__(value)
-        self.color = 0 #0 for black, 1 for one
+        self.color = None
+
 
 
 class RedBlackTree(BST):
-    pass
+    def __init__(self, value):
+        self.root = TreeNode(value)
+        self.root.color = Color.BLACK
+
+
+
 
 
 if __name__ == "__main__":
     rbt = RedBlackTree(1)
-    print(rbt)
+    print(rbt.root.color)
