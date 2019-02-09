49cb1e64ab8c88db9758f07c9b16e10422a2d044
anwarvic <mohamedanwarvic@gmail.com>
2019-02-09 13:46:18 +0200

    adding more test-cases in RedBlackTree and the last one showed a big bug... trying to fix it right now

--- Trees/red_black_tree.py
+++ Trees/red_black_tree.py
@@ -96,7 +96,7 @@ class RedBlackTree(BST):
         - case III: parent is 'red' and uncle is 'black'
         """
         # check if start_node is root (parent is None)
-        if not start_node.get_parent():
+        if start_node.get_parent() is None:
             return start_node
         # get basic info
         uncle = start_node.get_uncle()
@@ -114,20 +114,20 @@ class RedBlackTree(BST):
                 grandparent.set_color(Color.RED)
             # case III
             else:
-                # parent is left-child and node is left-child
+                # parent is left-child and start_node is left-child
                 if parent.is_left_child() and start_node.is_left_child():
                     grandparent.set_color(Color.RED)
                     parent.set_color(Color.BLACK)
                     grandparent = self.rotate_right(grandparent)
-                # parent is left-child and node is right-child
+                # parent is left-child and start_node is right-child
                 elif parent.is_left_child() and not start_node.is_left_child():
                     parent = self.rotate_left(parent)
                     grandparent.set_left(parent)
-                # parent is right-child and node is left-child
+                # parent is right-child and start_node is left-child
                 elif not parent.is_left_child() and start_node.is_left_child():
                     parent = self.rotate_right(parent)
                     grandparent.set_right(parent)
-                # parent is right-child and node is right-child
+                # parent is right-child and start_node is right-child
                 else:
                     grandparent.set_color(Color.RED)
                     parent.set_color(Color.BLACK)
@@ -138,32 +138,22 @@ class RedBlackTree(BST):
             return self.__recolor(parent)
 
 
-
-    def __insert(self, value):
-        """
-        When inserting a value, we set color as red and then re-color it
-        """
+    def insert(self, value):
         # insert new node
-        node = super().insert(value)
-        # cast into TreeNode-with-color
-        new_node = TreeNode(value) # red by default
-        parent = node.get_parent()
-        grandparent = parent.get_parent()
+        tmp_node = super().insert(value)
+        # create new TreeNode with red color
+        new_node = TreeNode(value) #red by default
+        parent = tmp_node.get_parent()
         # take care of node-parent connection
-        if value > parent.get_data():
+        if not parent or value == parent.get_data():
+            return
+        elif value > parent.get_data():
             parent.set_right(new_node)
         elif value < parent.get_data():
             parent.set_left(new_node)
-        # start recoloring when inserted node has a grandparent
-        if grandparent:
-            # recolor starting from new_node till grandparent
-            self.root = self.__recolor(new_node)
-
-
-    def insert(self, value):
-        # insert new value
-        self.__insert(value)
-        # make sure the root is black
+        # recolor starting from new_node till root
+        self.root = self.__recolor(new_node)
+        # root is black (isn't essential tho!!)
         self.root.set_color(Color.BLACK)
 
 
@@ -173,20 +163,37 @@ class RedBlackTree(BST):
 
 
 if __name__ == "__main__":
-    rbtree = RedBlackTree(8)
-    rbtree.insert(5)
-    rbtree.insert(15)
-    rbtree.insert(12)
-    rbtree.insert(19)
-    rbtree.insert(9)
-    rbtree.insert(13)
-    rbtree.insert(23)
-    rbtree.insert(10)
-    print(rbtree)
+    # # src: https://www.youtube.com/watch?v=eO3GzpCCUSg
+    # rbtree = RedBlackTree(8)
+    # rbtree.insert(8)
+    # rbtree.insert(5)
+    # rbtree.insert(15)
+    # rbtree.insert(12)
+    # rbtree.insert(19)
+    # rbtree.insert(9)
+    # rbtree.insert(13)
+    # rbtree.insert(23)
+    # rbtree.insert(10)
+    # print(rbtree)
 
-    # test special cases
+    # # test special cases
     # rbtree = RedBlackTree(15)
     # rbtree.insert(5)
     # rbtree.insert(1)
     # print(rbtree)
+
+    # # src: https://www.geeksforgeeks.org/red-black-tree-set-2-insert/
+    # rbtree = RedBlackTree(10)
+    # rbtree.insert(20)
+    # rbtree.insert(30)
+    # rbtree.insert(15)
+    # print(rbtree)
+
+    # src: http://www.btechsmartclass.com/data_structures/red-black-trees.html
+    rbtree = RedBlackTree(8)
+    rbtree.insert(18)
+    rbtree.insert(5)
+    rbtree.insert(15)
+    rbtree.insert(17)
+    print(rbtree)
 
