1c5b46bfda2bcd6177a8796a6dfaf78ac58a74c7
anwarvic <mohamedanwarvic@gmail.com>
2019-09-13 15:11:19 +0200

    insert() now works like a charm with a little bug

--- Trees/radix_trie.py
+++ Trees/radix_trie.py
@@ -6,7 +6,33 @@ class RadixTrie(Trie):
    
     ############################## INSERTION ##############################
     def insert(self, word):
-        pass]
+        assert type(word) == str, "You can insert String objects only!!"
+        assert len(word) > 0, "You can't insert any empty String!!"
+
+        if self.root.children == {}:
+            self.root.set_child(word[0], TrieNode(word))
+        else:
+            start_node = self.root
+            while(word):
+                ch = word[0]
+                child = start_node.get_child(ch)
+                if not child:
+                    start_node.set_child(word[0], TrieNode(word))
+                    return
+                else:
+                    child_data = child.get_data()
+                    idx = find_last_common_idx(child_data, word)
+                    # whole word is found
+                    if idx == len(word):
+                        return
+                    # child has the given word as prefix
+                    elif idx > len(word):
+                        child.data = word
+                        word = child_data
+                    # child has prefix of the word
+                    else:
+                        start_node = child
+                        word = word[idx:]
 
             
             
@@ -27,5 +53,9 @@ class RadixTrie(Trie):
 
 
 if __name__ == "__main__":
-    pass
+    t = RadixTrie()
+    t.insert("shear")
+    t.insert("she")
+    t.insert("shepard")
+    print(t)
     # print(t.root.children)
