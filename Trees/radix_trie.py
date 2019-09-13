013130b31fdae2d646190069cce98a9a5c4d9505
anwarvic <mohamedanwarvic@gmail.com>
2019-09-13 15:10:42 +0200

    created basic structure for RadixTrie class

---
+++ Trees/radix_trie.py
@@ -0,0 +1,31 @@
+from trie import TrieNode, Trie
+
+
+
+class RadixTrie(Trie):
+   
+    ############################## INSERTION ##############################
+    def insert(self, word):
+        pass]
+
+            
+            
+
+
+    ############################## FIND ##############################
+    def find(self, word):
+        pass
+
+
+    ######################### AUTO-COMPLETION #########################
+    def get_candidates(self, prefix=''):
+        pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
+    # print(t.root.children)
