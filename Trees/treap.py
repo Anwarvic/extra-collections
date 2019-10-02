39c29edfed1c2c756d1cf0b936447dab39be1c69
anwarvic <mohamedanwarvic@gmail.com>
2019-10-02 18:52:20 +0200

    adding rotation operation for the Treap

--- Trees/treap.py
+++ Trees/treap.py
@@ -7,6 +7,7 @@ class TreapNode(TreeNode):
         super().__init__(key)
         self.priority = randint(0, 1000)
 
+
     def get_priority(self):
         return self.priority
 
@@ -30,6 +31,24 @@ class Treap(BST):
             self.root = TreapNode(key)
 
     
+    ############################## ROTATION  ##############################    
+    def __rotate_left(self, start_node):
+        # print("Rotating Left")
+        middle = start_node.get_right()
+        middle.set_parent( start_node.get_parent() )
+        start_node.set_right(middle.get_left())
+        middle.set_left(start_node)
+        return middle
+
+    def __rotate_right(self, start_node):
+        # print("Rotating Right")
+        middle = start_node.get_left()
+        middle.set_parent( start_node.get_parent() )
+        start_node.set_left(middle.get_right())
+        middle.set_right(start_node)
+        return middle
+
+
     ############################## INSERTION ##############################
     def insert(self, ):
         pass
