e192832a66a318bb157d86973279934f2664e72f
anwarvic <mohamedanwarvic@gmail.com>
2019-10-10 19:12:45 +0200

    added insert() in Suffix Trie

--- Trees/suffix_trie.py
+++ Trees/suffix_trie.py
@@ -1,13 +1,39 @@
-from radix_trie import RadixTrie
+from radix_trie import TrieNode, RadixTrie
+
+
+
+class TrieNode(TrieNode):
+    def __init__(self, value):
+        super().__init__(value)
+        delattr(self, 'is_word')
+
+    def represent_data(self):
+        return self.data
+
+
 
 
 class SuffixTrie(RadixTrie):
-    pass
+    
+    ############################## INSERTION ##############################
+    def insert(self, word):
+        assert type(word) == str, "You can insert String objects only!!"
+        assert len(word) > 0, "You can't insert any empty String!!"
+        
+        # Ukkonen's algorithm
+        for idx in range(len(word)+1):
+            # create new node using given word
+            sub_word = word[idx:]+'$' #'$': string termination symbol
+            super()._insert(sub_word)
+
+
 
 
 
 if __name__ == "__main__":
     st = SuffixTrie()
-    st.insert("she")
-    st.insert("shepard")
+    st.insert("banana")
     print(st)
+
+    # st = SuffixTrie()
+    # st.insert("m")
