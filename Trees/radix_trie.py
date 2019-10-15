4a7850dad1ef824c857bd47dc4180022ee2fd0d0
anwarvic <mohamedanwarvic@gmail.com>
2019-10-16 00:15:11 +0200

    remove() method in RadixTrie can be inherited directly from Trie object... what a reliefgit add radix_trie.py

--- Trees/radix_trie.py
+++ Trees/radix_trie.py
@@ -47,34 +47,6 @@ class RadixTrie(Trie):
         self.nodes_count += 1
 
 
-    ############################## REMOVE ##############################
-    def remove(self, word):
-        assert type(word) == str, "You can remove String objects only!!"
-        # search for the word
-        start_node = self.root
-        while(word):
-            ch = word[0]
-            child = start_node.get_child(ch)
-            if not child:
-                return
-            else:
-                child_data = child.get_data()
-                if child_data == word[:len(child_data)]:
-                    start_node = child
-                    word = word[len(child_data):]
-                else:
-                    return
-                
-        # if word is found, clear it
-        if start_node.is_word:
-            start_node.is_word = False
-        while(not start_node.is_word and start_node.has_no_children()):
-            ch = start_node.get_data()[0]
-            parent = start_node.get_parent()
-            del parent.children[ch]
-            start_node = parent
-
-
     ######################### AUTO-COMPLETION #########################    
     def auto_complete(self, prefix=''):
         assert type(prefix) == str, "A character-sequence is expected!!"
@@ -164,11 +136,13 @@ if __name__ == "__main__":
     rt.insert("toast")
     rt.insert("slower")
     print(rt)
+    print(len(rt))
     print("slowlie" in rt)
     rt.remove("test")  # remove 'est' from tree
     rt.remove("slow") # remove is_word
     rt.remove("slowl") # do nothin'
     print(rt)
+    print(len(rt))
     print('='*50)
     print(rt.has_substring("slow"))
     print('slow' in rt)
