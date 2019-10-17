2c23941ab1661f5eea3249e7880dfea2b1aa4951
anwarvic <mohamedanwarvic@gmail.com>
2019-10-17 14:17:25 +0200

    has_substring() method in RadixTrie has some problems.. gonna fix it right now

--- Trees/radix_trie.py
+++ Trees/radix_trie.py
@@ -83,17 +83,17 @@ class RadixTrie(Trie):
 
 if __name__ == "__main__":
     # # src: https://en.wikipedia.org/wiki/Radix_tree?oldformat=true
-    # rt = RadixTrie()
-    # rt.insert("romane")
-    # rt.insert("romane")
-    # rt.insert("romanus")
-    # rt.insert("romulus")
-    # rt.insert("rubens")
-    # rt.insert("ruber")
-    # rt.insert("rubicon")
-    # rt.insert("rubicundus")
-    # print(rt)
-    # print('='*50)
+    rt = RadixTrie()
+    rt.insert("romane")
+    rt.insert("romane")
+    rt.insert("romanus")
+    rt.insert("romulus")
+    rt.insert("rubens")
+    rt.insert("ruber")
+    rt.insert("rubicon")
+    rt.insert("rubicundus")
+    print(rt)
+    print('='*50)
 
     rt = RadixTrie()
     rt.insert('s')
@@ -119,30 +119,30 @@ if __name__ == "__main__":
     print(rt.auto_complete("shep")) # ['shepard']
     print('='*50)
 
-    # rt = RadixTrie()
-    # rt.insert("test")
-    # rt.insert("toaster")
-    # rt.insert("toasting")
-    # rt.insert("slow")
-    # rt.insert("slowly")
-    # rt.insert("slowlier")
-    # rt.insert("toast")
-    # rt.insert("slower")
-    # print(rt)
-    # print(len(rt))
-    # print("slowlie" in rt)
-    # rt.remove("test")  # remove 'est' from tree
-    # rt.remove("slow") # remove is_word
-    # rt.remove("slowl") # do nothin'
-    # print(rt)
-    # print(len(rt))
-    # print('='*50)
-    # print(rt.has_substring("slow"))
-    # print('slow' in rt)
+    rt = RadixTrie()
+    rt.insert("test")
+    rt.insert("toaster")
+    rt.insert("toasting")
+    rt.insert("slow")
+    rt.insert("slowly")
+    rt.insert("slowlier")
+    rt.insert("toast")
+    rt.insert("slower")
+    print(rt)
+    print(len(rt))
+    print("slowlie" in rt)
+    rt.remove("test")  # remove 'est' from tree
+    rt.remove("slow") # remove is_word
+    rt.remove("slowl") # do nothin'
+    print(rt)
+    print(len(rt))
+    print('='*50)
+    print(rt.has_substring("slo"))
+    print('slow' in rt)
     
-    # # # sanity checks
-    # # rt = RadixTrie()
-    # # print(rt.find(''))
-    # # print(rt.find(2)) #throws error
+    # # sanity checks
+    # rt = RadixTrie()
+    # print(rt.find(''))
+    # print(rt.find(2)) #throws error
 
 
