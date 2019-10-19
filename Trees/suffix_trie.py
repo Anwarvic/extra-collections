5e012de00fe328e3e74ee1f4335f89f3b84d36bd
anwarvic <mohamedanwarvic@gmail.com>
2019-10-19 15:58:25 +0200

    SuffixTrie is not can't inherit from RadixTrie... it's a special RadixTrie... so I deleted former SuffixTrie and created another one which make more sense now

--- Trees/suffix_trie.py
+++ Trees/suffix_trie.py
@@ -2,32 +2,33 @@ from radix_trie import RadixTrie
 
 
 
-class SuffixTrie(RadixTrie):
-    
-    ############################## INSERTION ##############################
-    def insert(self, word):
-        assert type(word) == str, "You can insert String objects only!!"
-        assert len(word) > 0, "You can't insert any empty String!!"
-        
+class SuffixTrie:
+
+    def __init__(self, word):
+        assert type(word) == str, "The initial value must be a string!!"
+        assert len(word) > 0, "An empty string can't be used!!"
+        self.rt = RadixTrie()
         # Ukkonen's algorithm
         for idx in range(len(word)):
-            # create new node using given word
-            sub_word = word[idx:]
-            super().insert(sub_word)
+            self.rt.insert(word[idx:])
 
-    ############################## remove ##############################
-    def remove(self):
-        raise NotImplementedError("Can't remove from SuffixTries!!")
+
+    def __repr__(self):
+        return str(self.rt)
+    
+    ############################## PATTERN ##############################        
+    def hasSuffix(self, s):
+        """ Return  true    iff s   is  a   suffix  of  t   """
+        node    = self.followPath(s)
+        return  node    is not None and '$' in  node
 
 
 
 
 if __name__ == "__main__":
-    # st = SuffixTrie()
-    # st.insert("banana")
+    # st = SuffixTrie("banana")
     # print(st)
     # print(st.find('nan'))
 
-    st = SuffixTrie()
-    st.insert("minimize")
+    st = SuffixTrie("minimize")
     print(st)
