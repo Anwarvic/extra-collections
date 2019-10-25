baf7eddb57b7617dba509bbf5086628839bebe1b
anwarvic <mohamedanwarvic@gmail.com>
2019-10-25 19:58:58 +0200

    determining methods that will be implemented later in SuffixTrie

--- Trees/suffix_trie.py
+++ Trees/suffix_trie.py
@@ -1,3 +1,7 @@
+"""
+Suffix trees store information about a single string and exports a huge amount
+of structural information about that string.
+"""
 from radix_trie import RadixTrie
 from trie import Trie
 
@@ -21,8 +25,41 @@ class SuffixTrie:
     def __len__(self):
         return len(self.rt)
 
+
     def has_suffix(self, suffix):
         return self.rt.has_prefix(suffix)
+
+
+    def has_substr(self, substr):
+        return substr in self.rt
+
+
+    def get_lcs(self):
+        #TODO:stands for "longest Common Substring"
+        pass
+
+
+    def get_lrs(self):
+        """TODO
+        LRS stands for "longest Repeated Substring". lrs is the longest
+        substring that occurs at least twice.
+        """
+        pass
+
+
+    def get_longest_palindrome(self):
+        #TODO
+        pass
+
+
+    def get_lowest_common_ancestor(self):
+        #TODO
+        pass
+
+
+    def 
+
+
 
 
 
@@ -31,9 +68,26 @@ if __name__ == "__main__":
     # st = SuffixTrie("banana")
     # print(st)
     # print(st.find('nan'))
-
-    st = SuffixTrie("minimize")
-    # st = SuffixTrie("ATCGATCGA")
+    
+    st = SuffixTrie("ATCGATCGA")
     print(st)
+    print(st.get_longest_repeated_substr())
     print("Total Nodes:", len(st))
-    print(st.has_suffix('ize'))    
+
+
+    # st = SuffixTrie("minimize")
+    # print(st)
+    # print("Total Nodes:", len(st))
+    # print(st.has_suffix('ize'))    
+
+    # st = SuffixTrie("nonsense")
+    # print(st)
+    # print("Total Nodes:", len(st))
+    # print(st.has_substr("se"))
+    # print(st.get_longest_repeated_substr())
+
+
+    # st = SuffixTrie("ABABABA")
+    # print(st)
+    # print(st.get_longest_repeated_substr())    
+
