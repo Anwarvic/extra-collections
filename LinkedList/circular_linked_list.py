e6e2c8a832e3f2348e0e6e610c8f5ab6e26a8809
anwarvic <mohamedanwarvic@gmail.com>
2019-11-26 14:45:46 +0200

    started refactoring Circular Linked List

--- LinkedList/circular_linked_list.py
+++ LinkedList/circular_linked_list.py
@@ -1,105 +1,44 @@
-class Node():
-    """Basic object for the Node used for Circular linked lists"""
-    def __init__(self, item=None):
-        self.data = item
-        self.next = None
-
-    def __repr__(self):
-        """Represents Node object as a string"""
-        data = self.data
-        nxt = self.next.data if self.next else None
-        return "Node: (item: {}, next: {})".format(data, nxt)
+from linked_list import Node, LinkedList
 
 
 
-class CircularLinkedList():
+class CircularLinkedList(LinkedList):
     """Basic object for the Circular Linked List"""
     def __init__(self, item=None):
-        self.head = Node(item)
-        self.head.next = self.head
-        self.length = 1 if item else 0
-
+       super().__init__(item)
+       self.head.set_next(self.head)
 
     def __repr__(self):
         """Represents the Circular linked list as a string"""
-        # NOTE: complexity of + operator is O(1) in lists and O(n) in string
-        first_line = ['┌']
-        second_line = ['│']
-        third_line = ['└']
-        fourth_line = ''
-        fifth_line = ''
-        pointer = self.head
-        while(pointer.next != self.head):
-            item = pointer.data
-            width = len(str(item))+2 #2: for a space before & after an item
-            first_line += (['─']*width) + ['┬']
-            second_line += [" {} →".format(item)]
-            third_line += (['─']*width) + ['┴']
-            pointer = pointer.next
-        # add last item
-        item = pointer.data
-        width = len(str(item))+2 #2: for a space before & after an item
-        first_line += (['─']*width) + ['┬']
-        second_line += [" {} →".format(item)]
-        third_line += (['─']*width) + ['┴']
+        # represent the Linked List
+        first_line, second_line, third_line = \
+            super()._print_linked_list(stop_node=self.head)
+        # check status
+        if self.is_empty():
+            return "{}\n{}\n{}".format(\
+            ''.join(first_line), ''.join(second_line), ''.join(third_line))
         # backtrace representation
-        second_line += [' ┐']
-        third_line += [' │']
-        left_offset = (len(str(self.head.data))+3)//2
-        remaining = len(first_line) - left_offset
-        fourth_line = (' '*left_offset) + '^' + (' '*(remaining)) + '│'
-        fifth_line = (' '*left_offset) + '└' + ('─'*(remaining)) + '┘'
-        return "{}\n{}\n{}\n{}\n{}".format(\
-            "".join(first_line), "".join(second_line), "".join(third_line),\
-            fourth_line, fifth_line)
-
-
-    def __len__(self):
-        """Gets the length of the linked list with complexity of O(1)"""
-        return self.length
-
-
-    def __fix_index(self, idx):
-        """
-        private method to return the appropriate index for the circular linked
-        list. If the given index is negative, then it converts it to a positive one.
-        It raises a TypeError if the index isn't integer.
-        """
-        if type(idx) != int:
-            msg = "idx must be an integer!"
-            raise TypeError(msg)
-        if self.length == 0:
-            return 0
         else:
-            idx = idx % (self.length)
-            return idx
-
+            second_line += [' ┐']
+            third_line += [' │']
+            left_offset = (len(str(self.head.data))+3)//2
+            remaining = len(first_line) - left_offset
+            fourth_line = (' '*left_offset) + '↑' + (' '*(remaining)) + '│'
+            fifth_line = (' '*left_offset) + '└' + ('─'*(remaining)) + '┘'
+            return "{}\n{}\n{}\n{}\n{}".format(\
+                "".join(first_line), "".join(second_line), "".join(third_line),\
+                fourth_line, fifth_line)
+        
 
     def __getitem__(self, idx):
         """Retrieves the element at the given index."""
-        # sanity check over given index
-        idx = self.__fix_index(idx)
-        pointer = self.head
-        # handle edge case
-        if idx == 0:
-            return self.head
-        # iterate over the linked list
-        counter = 0
-        pointer = self.head
-        while(pointer.next != self.head):
-            counter += 1
-            pointer = pointer.next
-            if counter == idx:
-                return pointer
+        idx = idx % (self.length)
+        return super().__getitem__(idx)
 
 
-    def __setitem__(self, idx):
-        pass
-
-
-    def is_empty(self):
-        """Checks if circular linked list is empty"""
-        return self.length == 0
+    def __setitem__(self, idx, item):
+        idx = idx % (self.length)
+        super().__setitem__(idx, item)
 
 
     def add_front(self, item):
@@ -116,17 +55,8 @@ class CircularLinkedList():
 
 
     def add_end(self, item):
-        """Adds node at the head of the circular linked list"""
-        if self.length == 0:
-            self.head.data = item
-        else:
-            pointer = self.head
-            while(pointer.next != self.head):
-                pointer = pointer.next
-            new_node = Node(item)
-            new_node.next = self.head
-            pointer.next = new_node
-        self.length += 1
+        raise NotImplementedError(\
+            "You can't insert at the end of a Circular Linked List!!")
 
 
     def remove_front(self):
@@ -146,16 +76,8 @@ class CircularLinkedList():
 
     def remove_end(self):
         """Removes the circular linked list tail with complexity of O(n)"""
-        if self.length == 1:
-            self.head.data = None
-            self.length -= 1
-        elif self.length > 0:
-            pointer = self.head
-            while(pointer.next.next != self.head):
-                pointer = pointer.next
-            #pointer is now at the second last item
-            pointer.next = self.head
-            self.length -= 1
+        raise NotImplementedError(\
+            "You can't remove from the end of a Circular Linked List!!")
 
 
     def insert(self, idx, item):
@@ -182,13 +104,6 @@ class CircularLinkedList():
         pass
 
 
-    def clear(self):
-        """Removes all nodes within the linked list with complexity of O(1)"""
-        self.head = Node()
-        self.head.next = self.head
-        self.length = 0
-
-
     def reverse(self):
         """Reverses the whole linked list with complexity of O(n)"""
         rev = CircularLinkedList()
@@ -204,27 +119,27 @@ class CircularLinkedList():
 
 if __name__ == "__main__":
     l = CircularLinkedList()
-    l.insert(100, 'answer')
-    l.add_end('item')     #answer item
-    l.insert(2, 'item2')  #answer item item2
-    l.insert(-1, 'item3') #answer item item3 item2
+    # l.insert(100, 'answer')
+    # l.add_end('item')     #answer item
+    # l.insert(2, 'item2')  #answer item item2
+    # l.insert(-1, 'item3') #answer item item3 item2
     # l.insert(-20, 'item4') #item item2 item3
     print(l)
-    l.add_front(100)
-    l.add_front(700)
-    l.add_end(90)
-    l.add_front("B")
-    print(l)
-    print(l[-10])
+    # l.add_front(100)
+    # l.add_front(700)
+    # l.add_end(90)
+    # l.add_front("B")
+    # print(l)
+    # print(l[-10])
 
-    l.insert(10, 'item')
-    print(l)
+    # l.insert(10, 'item')
+    # print(l)
 
-    rev = l.reverse()
-    rev.remove_end()
-    print(rev)
-    print(rev[-1])
-    print(rev.is_empty())
+    # rev = l.reverse()
+    # rev.remove_end()
+    # print(rev)
+    # print(rev[-1])
+    # print(rev.is_empty())
     
-    rev.remove_front()
-    print(rev.is_empty())
+    # rev.remove_front()
+    # print(rev.is_empty())
