6ad7527f68dcff4fef94c84c89dcc95e2355005a
anwarvic <mohamedanwarvic@gmail.com>
2020-01-06 22:28:49 +0200

    Deque is working fine now

--- LinkedList/deque.py
+++ LinkedList/deque.py
@@ -13,40 +13,34 @@ class Deque(Queue):
 
     def append_left(self, item):
         """Insert value into the front of the Deque"""
-        if self.max_capacity and len(self) == self.max_capacity:
-            self.remove_right()
-        self.container.insert(0, item)
+        super().enqueue(item)
 
 
     def append_right(self, item):
         """Insert value into the end of the Deque"""
-        if self.max_capacity and len(self) == self.max_capacity:
-            self.remove_left()
-        self.container.append(item)
+        if len(self.container) >= self.max_capacity:
+            self.container.remove_end()
+        self.container.add_end(item)
 
 
-    def remove_left(self):
+    def pop_left(self):
         """Removes value from the Deque (first-inserted item)"""
-        return self.container.pop(0)
+        return self.container.remove_front()
 
 
-    def remove_right(self):
+    def pop_right(self):
         """Removes value from the Deque (most recent)"""
-        return self.container.pop()
+        return super().dequeue()
 
-
-    def peek(self):
-        """Returns the Deque recent item"""
-        return self.container[-1]
 
 
 
 
 if __name__ == "__main__":
     q = Deque(max_capacity=3)
-    q.append_left(0) #0
+    q.append_left(0)    #0
     q.append_right(1)   #0 1
     q.append_right(8)   #0 1 8
-    q.append_left(9) #9 0 1
+    q.append_left(9)    #9 0 1
     print(q)
     
