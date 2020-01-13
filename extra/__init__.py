d5e7c4638f4648a132b3219c5d37348aa32d7ddb
anwarvic <mohamedanwarvic@gmail.com>
2020-01-14 01:17:58 +0200

    moved __init__() to

---
+++ extra/__init__.py
@@ -0,0 +1,24 @@
+# lists
+from extra.lists.linked_list import LinkedList as LinkedList
+from extra.lists.doubly_linked_list import DoublyLinkedList as DoublyLinkedList
+# from extra.lists.circular_linked_list import CircularLinkedList as CircularLinkedList
+# from extra.lists.skip_list import SkipList as SkipList
+# from extra.lists.stack import Stack as Stack
+# from extra.lists.queue import Queue as Queue
+# from extra.lists.priority_queue import PriorityQueue as PriorityQueue
+
+
+# # trees
+# from extra.trees.tree import Tree as Tree
+# from extra.trees.binary_tree import BinaryTree as BinaryTree
+# from extra.trees.bst import BST as BST
+# from extra.trees.splay_tree import SplayTree as SplayTree
+# from extra.trees.avl import AVL as AVL
+# from extra.trees.red_black_tree import RedBlackTree as RedBlackTree
+# from extra.trees.heap import MinHeap as MinHeap
+# from extra.trees.heap import MaxHeap as MaxHeap
+# from extra.trees.treap import Treap as Treap
+# from extra.trees.trie import Trie as Trie
+# from extra.trees.radix_trie import RadixTrie as RadixTrie
+# from extra.trees.suffix_trie import SuffixTrie as SuffixTries
+
