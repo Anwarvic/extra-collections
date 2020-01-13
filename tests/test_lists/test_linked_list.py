436744dfa70ee4402b88d335764edc0a9f312541
anwarvic <mohamedanwarvic@gmail.com>
2020-01-14 01:40:34 +0200

    adding a few more test-cases in Linked List

--- tests/test_lists/test_linked_list.py
+++ tests/test_lists/test_linked_list.py
@@ -1,6 +1,30 @@
 import pytest
-from extra.lists.linked_list import LinkedList as LinkedList
+from extra.lists.linked_list import Node, LinkedList
+
+
+
+def test_empty_node():
+    pass
+
+
+def test_not_empty_node():
+    pass
+
 
 def test_empty_list():
     ll = LinkedList()
+    assert isinstance(ll.head, Node)
     assert ll.head.get_data() == None
+    assert ll.head.next == None
+    assert not isinstance(ll.head.next, Node)
+    assert ll.length == 0
+    assert str(ll) == "┌─\n│\n└─"
+    assert ll.is_empty()
+    assert ll.to_list() == []
+
+
+
+def test_empty_list_given_empty_node():
+    ll = LinkedList(Node())
+    assert ll.head.get_data() == None
+    assert ll.length == 0
