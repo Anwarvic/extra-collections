95df9047b0aff6fb1e4e640ed52df5d659c9fb58
anwarvic <mohamedanwarvic@gmail.com>
2020-01-14 01:32:34 +0200

    adding first test file

--- tests/test_lists/test_linked_list.py
+++ tests/test_lists/test_linked_list.py
@@ -1,6 +1,6 @@
 import pytest
-from extra.lists.linked_list import LinkedList
+from extra.lists.linked_list import LinkedList as LinkedList
 
 def test_empty_list():
     ll = LinkedList()
-    assert ll.head == None
+    assert ll.head.get_data() == None
