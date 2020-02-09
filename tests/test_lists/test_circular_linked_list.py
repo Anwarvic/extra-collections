572a50d27518bc8e200657e6879e1ba998572f89
anwarvic <mohamedanwarvic@gmail.com>
2020-02-09 17:13:26 +0200

    addine more test method for CircularLinkedList

--- tests/test_lists/test_circular_linked_list.py
+++ tests/test_lists/test_circular_linked_list.py
@@ -4,6 +4,7 @@ import pytest
 from tests import *
 from extra.lists.linked_list import Node, LinkedList
 from extra.lists.circular_linked_list import CircularLinkedList
+from extra.lists.doubly_linked_list import DoublyNode, DoublyLinkedList
 
 
 def test_creating_linked_list_from_constructor():
@@ -15,6 +16,8 @@ def test_creating_linked_list_from_constructor():
     assert cll.head.get_next() == cll.head.next == cll.head
     assert len(cll) == cll.length == 1
     assert cll.to_list() == [item.get_data() for item in cll] == [val]
+    # Using Node (should raise a Warning!)
+    cll = CircularLinkedList( DoublyNode(get_value()) )
     # Using Node
     val = get_value()
     node = Node(val)
@@ -25,3 +28,34 @@ def test_creating_linked_list_from_constructor():
     cll.head.get_next() == cll.head.next == cll.head
     assert len(cll) == cll.length == 1
     assert cll.to_list() == [item.get_data() for item in cll] == [val]
+
+
+def test_creating_doubly_linked_list_from_iterable():
+    # Using from_iterable (small length)
+    lst = get_list()
+    cll = CircularLinkedList.from_iterable(lst)
+    assert cll.head.get_data() == lst[0]
+    assert len(cll) == cll.length == len(lst)
+    assert cll.to_list() == [item.get_data() for item in cll] == lst
+    # Using from_iterable (has None)
+    with pytest.raises(TypeError):
+        CircularLinkedList.from_iterable([1, 2, None, 3])
+        CircularLinkedList().add_end(LinkedList(10))
+        CircularLinkedList().add_front(DoublyLinkedList(10))
+        CircularLinkedList().add_front(CircularLinkedList(10))
+    # Using from_iterable (big length)
+    lst = get_list(length = 10000)
+    cll = CircularLinkedList.from_iterable(lst)
+    assert cll.head.get_data() == lst[0]
+    assert len(cll) == cll.length == len(lst)
+    assert cll.to_list() == [item.get_data() for item in cll] == lst
+    for _ in range(100): #check random indices
+        idx = get_pos_int(b=10000)
+        assert cll[idx].get_data() == lst[idx]
+    # Using Linked List
+    lst = get_list()
+    tmp_cll = CircularLinkedList.from_iterable(lst)
+    cll = CircularLinkedList.from_iterable(tmp_cll)
+    assert cll == tmp_cll
+    assert len(cll) == cll.length == len(lst)
+    assert cll.to_list() == [item.get_data() for item in cll] == lst
