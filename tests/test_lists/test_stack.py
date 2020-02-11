0abfd9fb548fafb0b7e34e1e990fdc3c13618285
anwarvic <mohamedanwarvic@gmail.com>
2020-02-11 13:28:50 +0200

    adding one more test function for Stack

--- tests/test_lists/test_stack.py
+++ tests/test_lists/test_stack.py
@@ -10,9 +10,31 @@ def test_creating_stack():
     assert s.max_capacity == float("inf")
     assert s.container == []
     assert len(s) == 0
+    assert s.is_empty()
     assert s.pop() == None
     with pytest.raises(IndexError):
         s.peek()
     
 
+def test_creating_stack_with_random_numbers():
+    # stack with random values
+    lst = get_list(length=100)
+    s = Stack()
+    for i in lst:
+        s.push(i)
+    assert s.container == lst
+    assert len(s) == 0
+    assert s.peek() == lst[-1]
+    assert s.is_empty() == False
+    for _ in range(len(lst)):
+        assert s.peek() == lst[-1]
+        assert s.pop() == lst.pop()
+    assert len(s) == 0
+    assert s.is_empty()
+    assert s.pop() == None
+    
+
+
+def test_push_method():
+    pass
 
