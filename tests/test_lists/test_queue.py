0de1eaee71d0b1fba020ecaf8d494db012bd86cb
anwarvic <mohamedanwarvic@gmail.com>
2020-02-12 16:09:08 +0200

    added first test case for Queue

--- tests/test_lists/test_queue.py
+++ tests/test_lists/test_queue.py
@@ -5,99 +5,30 @@ from extra.lists.queue import Queue
 
 
 
-# def test_creating_queue():
-#     # empty stack
-#     s = Stack()
-#     assert s.max_capacity == float("inf")
-#     assert s.container == []
-#     assert len(s) == 0
-#     assert s.is_empty()
-#     assert s.pop() == None
-#     with pytest.raises(IndexError):
-#         s.peek()
-#     s.clear() #not to through any errors
-#     assert s.max_capacity == float("inf")
-#     # empty stack with max_capacity == 0
-#     s = Stack(max_capacity=0)
-#     assert s.max_capacity == 0
-#     assert s.container == []
-#     assert len(s) == 0
-#     assert s.is_empty()
-#     assert s.pop() == None
-#     with pytest.raises(IndexError):
-#         s.peek()
-#     with pytest.raises(OverflowError):
-#         s.push(get_value() )
-#     s.clear() #not to through any errors
-#     assert s.max_capacity == 0
+def test_creating_queue():
+    # empty stack
+    q = Queue()
+    assert q.max_capacity == float("inf")
+    assert len(q) == 0
+    assert q.is_empty()
+    assert q.dequeue() == None
+    with pytest.raises(IndexError):
+        q.get_first()
+    q.clear() #not to through any errors
+    assert q.max_capacity == float("inf")
+    assert not q.is_full()
+    # empty stack with max_capacity == 0
+    q = Queue(max_capacity=0)
+    assert q.max_capacity == 0
+    assert len(q) == 0
+    assert q.is_empty()
+    assert q.dequeue() == None
+    with pytest.raises(IndexError):
+        q.get_first()
+    with pytest.warns(UserWarning):
+        q.enqueue(get_value() )
+    q.clear() #not to through any errors
+    assert q.max_capacity == 0
+    assert q.is_full()
 
 
-# def test_stack_with_max_capacity():
-#     cap = get_pos_int()
-#     lst = get_list(length=cap)
-#     s = Stack(max_capacity= cap)
-#     assert s.max_capacity == cap
-#     for i in lst:
-#         s.push(i)
-#     assert s.container == lst
-#     assert len(s) == cap
-#     assert not s.is_empty()
-#     with pytest.raises(OverflowError):
-#         s.push(get_value())
-#     for i in range(cap):
-#         assert s.pop() == lst.pop()
-#     assert len(s) == 0
-#     assert s.is_empty()
-#     s.push(get_value())
-#     # test max capacity after clear
-#     s.clear()
-#     assert s.max_capacity == cap
-
-
-# def test_creating_stack_with_random_numbers():
-#     # stack with random values
-#     lst = get_list(length=100)
-#     s = Stack()
-#     for i in lst:
-#         s.push(i)
-#     assert s.container == lst
-#     assert len(s) == len(lst)
-#     assert s.peek() == lst[-1]
-#     assert s.is_empty() == False
-#     for _ in range(len(lst)):
-#         assert s.peek() == lst[-1]
-#         assert s.pop() == lst.pop()
-#     assert len(s) == 0
-#     assert s.is_empty()
-#     assert s.pop() == None
-
-
-# def test_stack_with_known_values():
-#     s = Stack(max_capacity=3)
-#     s.push(2)
-#     s.push(40)
-#     s.push(800)
-#     assert len(s) == 3
-#     assert s.container == [2, 40, 800]
-#     with pytest.raises(OverflowError):
-#         s.push(16000)
-#     assert s.peek() == 800
-#     assert s.pop() == 800
-#     assert s.container == [2, 40]
-#     assert not s.is_empty()
-#     s.clear()
-#     assert s.is_empty()
-#     assert s.max_capacity == 3
-
-
-# def test_push_method():
-#     s = Stack()
-#     with pytest.raises(TypeError):
-#         s.push(None)
-#     with pytest.raises(ValueError):
-#         s.push('')
-#     s.push(get_value())
-#     s.push(get_int())
-#     s.push(get_string())
-#     s.push(get_float())
-#     s.push(get_list())
