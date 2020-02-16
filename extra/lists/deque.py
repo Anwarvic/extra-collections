5ebdee8b3de2bb838f478d3a3cbf10c4f8614736
anwarvic <mohamedanwarvic@gmail.com>
2020-02-16 18:04:58 +0200

    refactored Deque() class, gonna test it now

--- extra/lists/deque.py
+++ extra/lists/deque.py
@@ -27,27 +27,32 @@ class Deque(Queue):
 
 
     ############################# APPEND ##############################
-    def append_left(self, item):
+    def append_first(self, item):
         """Insert value into the front of the Deque"""
+        super()._validate_item(item)
+        if self.is_full():
+            self.container.remove_end()
+        self.container.add_front(item)
+
+
+    def append_last(self, item):
+        """Insert value into the end of the Deque"""
         super().enqueue(item)
 
 
-    def append_right(self, item):
-        """Insert value into the end of the Deque"""
-        if len(self.container) >= self.max_capacity:
-            self.container.remove_end()
-        self.container.add_end(item)
-
-
     ############################## POP ###############################
-    def pop_left(self):
+    def pop_first(self):
         """Removes value from the Deque (first-inserted item)"""
-        return self.container.remove_front()
-
-
-    def pop_right(self):
-        """Removes value from the Deque (most recent)"""
         return super().dequeue()
+
+
+    def pop_last(self):
+        """Removes value from the Deque (most recent)"""
+        if self.is_empty():
+            warnings.warn(f"Dequeuing from an empty {self.__name__()}!!")
+            return None
+        else:
+            return self.container.remove_end().get_data()
 
 
 
