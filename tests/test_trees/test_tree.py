5fb422d2594a19b8d8847c49c7efb1db3dc0f653
anwarvic <mohamedanwarvic@gmail.com>
2020-02-26 13:48:38 +0200

    added dummy test in tree for later

--- tests/test_trees/test_tree.py
+++ tests/test_trees/test_tree.py
@@ -17,3 +17,31 @@ def test_treenode():
     TreeNode(get_value())
     TreeNode(get_list())
 
+
+# def test_tree_with_known_values():
+#     # create Simpsons Family tree
+#     root = TreeNode('TheSimpsons')
+#     # homer-side
+#     abraham = TreeNode('Abraham + Mona')
+#     herb = TreeNode('Herb')
+#     homer = TreeNode('Homer')
+#     abraham.children = [herb, homer]
+    
+#     # marge-side
+#     jackie = TreeNode('Clancy + Jackie')
+#     marge = TreeNode('Marge')
+#     patty = TreeNode('Patty')
+#     selma = TreeNode('Selma')
+#     ling = TreeNode('Ling')
+#     selma.children = [ling]
+#     jackie.children = [marge, patty, selma]
+
+#     # homer-marge children
+#     bart = TreeNode('Bart')
+#     lisa = TreeNode('Lisa')
+#     maggie = TreeNode('Maggie')
+#     homer.children = [bart, lisa, maggie]
+#     marge.children = homer.children
+#     # set root
+#     root.children = [abraham, jackie]
+#     t = Tree(root)
