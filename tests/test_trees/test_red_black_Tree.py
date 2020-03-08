d6f562bbd0facc1be01821bd7f4d86107629d533
anwarvic <mohamedanwarvic@gmail.com>
2020-03-08 21:39:48 +0200

    adding one more test case for RedBlackTrees

--- tests/test_trees/test_red_black_Tree.py
+++ tests/test_trees/test_red_black_Tree.py
@@ -397,3 +397,32 @@ def test_double_black_left_left_case():
     assert rbtree.root.get_right().get_right() is None
 
 
+def test_double_black_left_right_case():
+    # test case (left-right)
+    rbtree = RedBlackTree(40)
+    rbtree.insert(30)
+    rbtree.insert(50)
+    rbtree.insert(35)
+    rbtree.remove(50)
+    print(rbtree)
+    # test tree characteristics
+    assert verify_bst_rules(rbtree.root)
+    assert rbtree.is_balanced()
+    assert len(rbtree) == 3
+    assert rbtree.count_leaf_nodes() == 2
+    assert rbtree.get_max() == 40
+    assert rbtree.get_min() == 30
+    assert rbtree.get_height() == 1
+    # check data/colors
+    assert isinstance(rbtree.root, RedBlackNode)
+    assert rbtree.root.get_data() == 35
+    assert rbtree.root.get_color() == Color.BLACK
+    assert rbtree.root.get_left().get_data() == 30
+    assert rbtree.root.get_left().get_color() == Color.BLACK
+    assert rbtree.root.get_left().get_left() is None
+    assert rbtree.root.get_left().get_right() is None
+
+    assert rbtree.root.get_right().get_data() == 40
+    assert rbtree.root.get_right().get_color() == Color.BLACK
+    assert rbtree.root.get_right().get_left() is None
+    assert rbtree.root.get_right().get_left() is None
