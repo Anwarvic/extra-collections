b9cf7bb885f29ce42f5b0b51a21b8f118ad5d2a8
anwarvic <mohamedanwarvic@gmail.com>
2020-03-13 09:59:39 +0200

    handles some issues regarding initializing the class... it's OK now

--- extra/trees/heap.py
+++ extra/trees/heap.py
@@ -1,4 +1,3 @@
-from abc import ABC, abstractmethod
 from extra.trees._heap import Heap
 
 
@@ -7,6 +6,10 @@ from extra.trees._heap import Heap
 class MinHeap(Heap):
     def __name__(self):
         return "extra.MinHeap()"
+    
+
+    def __init__(self, value=None):
+        super().__init__(value)
     
 
     def get_min(self):
@@ -31,6 +34,10 @@ class MinHeap(Heap):
 class MaxHeap(Heap):
     def __name__(self):
         return "extra.MinHeap()"
+
+
+    def __init__(self, value=None):
+        super().__init__(value)
 
 
     def get_min(self):
@@ -60,60 +67,60 @@ if __name__ == "__main__":
     # h = Heap()
 
     # test iteration
-    heap = MinHeap([6, 2, 7, 1])
+    heap = MinHeap.heapify([6, 2, 7, 1])
     print(heap, '\n')
     for node in heap:
         print(node)
     print('='*50)
 
-    #####################################################
-    # test MinHeap
-    heap = MinHeap([1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17, 90, 100, 102, 190])
-    heap.remove(102)
-    heap.remove(4)
-    heap.remove(1)
-    print(heap)
-    print("Min value:", heap.get_min())
-    print("Max value:", heap.get_max())
-    print("Heap length:", len(heap))
-    print('='*50)
+    # #####################################################
+    # # test MinHeap
+    # heap = MinHeap([1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17, 90, 100, 102, 190])
+    # heap.remove(102)
+    # heap.remove(4)
+    # heap.remove(1)
+    # print(heap)
+    # print("Min value:", heap.get_min())
+    # print("Max value:", heap.get_max())
+    # print("Heap length:", len(heap))
+    # print('='*50)
 
-    #####################################################
-    # test MaxHeap
-    heap = MaxHeap([1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17, 90, 100, 102, 190])
-    heap.remove(102)
-    heap.remove(100)
-    heap.remove(190)
-    print(heap)
-    print("Min value:", heap.get_min())
-    print("Max value:", heap.get_max())
-    print("Heap length:", len(heap))
-    print('='*50)
+    # #####################################################
+    # # test MaxHeap
+    # heap = MaxHeap([1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17, 90, 100, 102, 190])
+    # heap.remove(102)
+    # heap.remove(100)
+    # heap.remove(190)
+    # print(heap)
+    # print("Min value:", heap.get_min())
+    # print("Max value:", heap.get_max())
+    # print("Heap length:", len(heap))
+    # print('='*50)
 
-    #####################################################
-    # test insert
-    heap = MinHeap(35)
-    heap.insert(33)
-    heap.insert(42)
-    heap.insert(10)
-    heap.insert(14)
-    heap.insert(19)
-    heap.insert(27)
-    heap.insert(44)
-    heap.insert(26)
-    heap.insert(31)
-    print(heap)
-    print('='*50)
+    # #####################################################
+    # # test insert
+    # heap = MinHeap(35)
+    # heap.insert(33)
+    # heap.insert(42)
+    # heap.insert(10)
+    # heap.insert(14)
+    # heap.insert(19)
+    # heap.insert(27)
+    # heap.insert(44)
+    # heap.insert(26)
+    # heap.insert(31)
+    # print(heap)
+    # print('='*50)
 
-    heap = MaxHeap(35)
-    heap.insert(33)
-    heap.insert(42)
-    heap.insert(10)
-    heap.insert(14)
-    heap.insert(19)
-    heap.insert(27)
-    heap.insert(44)
-    heap.insert(26)
-    heap.insert(31)
-    print(heap)
+    # heap = MaxHeap(35)
+    # heap.insert(33)
+    # heap.insert(42)
+    # heap.insert(10)
+    # heap.insert(14)
+    # heap.insert(19)
+    # heap.insert(27)
+    # heap.insert(44)
+    # heap.insert(26)
+    # heap.insert(31)
+    # print(heap)
     
