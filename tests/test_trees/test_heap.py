c0cabb77151dac9c63a8cabe3a64ff7c326e51fc
anwarvic <mohamedanwarvic@gmail.com>
2020-03-18 12:24:05 +0200

    adding first test-case for MinHeap

--- tests/test_trees/test_heap.py
+++ tests/test_trees/test_heap.py
@@ -1,9 +1,24 @@
 import pytest
 from tests import *
+from extra.trees._heap import Heap
 from extra.trees.heap import MinHeap, MaxHeap
 
 
 
 
 def test_creating_heap():
-    pass
+    with pytest.raises(TypeError):
+        _ = Heap()
+
+
+def test_heapify_of_min_heap_small_example():
+    lst = [6, 2, 7, 1]
+    heap = MinHeap.heapify(lst)
+    assert heap.to_list() == [1, 2, 7, 6]
+    assert len(heap) == len(lst)
+    assert heap.get_max() == max(lst)
+    assert heap.get_min() == min(lst)
+    for node, value in zip(heap, [1, 2, 7, 6]):
+        assert node.get_data() == value
+
+
