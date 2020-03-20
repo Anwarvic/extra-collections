742dc2a67a12d0e71074590ded37d32373598e6b
anwarvic <mohamedanwarvic@gmail.com>
2020-03-20 14:56:46 +0200

    MinHeap and MaxHeap both raise IndexError when we use get_min()/get_max() while the heap is empty

--- extra/trees/heap.py
+++ extra/trees/heap.py
@@ -13,10 +13,14 @@ class MinHeap(Heap):
     
 
     def get_min(self):
+        if self.is_empty():
+            raise IndexError("Can't get the minimum out of an empty Heap!!")
         return self._heap[0]
 
 
     def get_max(self):
+        if self.is_empty():
+            raise IndexError("Can't get the maximum out of an empty Heap!!")
         last_half = self._heap[len(self)//2:]
         return max(last_half)
 
@@ -41,11 +45,15 @@ class MaxHeap(Heap):
 
 
     def get_min(self):
+        if self.is_empty():
+            raise IndexError("Can't get the minimum out of an empty Heap!!")
         last_half = self._heap[len(self)//2:]
         return min(last_half)
 
 
     def get_max(self):
+        if self.is_empty():
+            raise IndexError("Can't get the maximum out of an empty Heap!!")
         return self._heap[0]
 
 
