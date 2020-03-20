2dcbe6f019c3af5dc6f759b760e64026cd8c2270
anwarvic <mohamedanwarvic@gmail.com>
2020-03-20 16:45:25 +0200

    finished testing heap classes

--- tests/test_trees/test_heap.py
+++ tests/test_trees/test_heap.py
@@ -105,6 +105,10 @@ def test_heap_with_same_value():
         length = get_pos_int()
         lst = [val]*length
         heap = Heap.heapify(lst)
+        if Heap == MinHeap:
+            assert verify_min_heap(heap._transform().root)
+        elif Heap == MaxHeap:
+            assert verify_max_heap(heap._transform().root)
         assert not heap.is_empty()
         assert heap.to_list() == lst
         assert len(heap) == len(lst)
@@ -124,13 +128,11 @@ def test_heap_with_same_value():
 def test_heapify_of_min_heap_small_example():
     lst = [6, 2, 7, 1]
     heap = MinHeap.heapify(lst)
+    assert verify_min_heap(heap._transform().root)
     assert not heap.is_empty()
-    assert heap.to_list() == [1, 2, 7, 6]
     assert len(heap) == len(lst)
     assert heap.get_max() == max(lst)
     assert heap.get_min() == min(lst)
-    for node, value in zip(heap, [1, 2, 7, 6]):
-        assert node.get_data() == value
     for num in lst:
         assert num in heap
     assert 19 not in heap
@@ -143,8 +145,8 @@ def test_heapify_of_min_heap_small_example():
 def test_heapify_of_min_heap_big_example():
     lst = [1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17, 90, 100, 102, 190]
     heap = MinHeap.heapify(lst)
-    assert heap.to_list() == lst
     assert len(heap) == len(lst)
+    assert verify_min_heap(heap._transform().root)
     assert heap.get_max() == max(lst)
     assert heap.get_min() == min(lst)
     for node, value in zip(heap, lst):
@@ -172,25 +174,26 @@ def test_insert_remove_min_heap():
     assert not heap.is_empty()
     assert heap.get_min() == 10
     assert heap.get_max() == 44
-    assert heap.to_list() == [10, 14, 19, 26, 31, 42, 27, 44, 35, 33]
+    assert verify_min_heap(heap._transform().root)
     heap.remove(0) #does nothing
     assert len(heap) == 10
     heap.remove(10)
     assert len(heap) == 9
     assert heap.get_min() == 14
     assert heap.get_max() == 44
+    assert verify_min_heap(heap._transform().root)
     heap.remove(44)
     assert len(heap) == 8
     assert heap.get_min() == 14
     assert heap.get_max() == 42
-    assert heap.to_list() == [14, 26, 19, 33, 31, 42, 27, 35]
+    assert verify_min_heap(heap._transform().root)
 
 
 def test_heapify_of_max_heap_small_example():
     lst = [6, 2, 7, 1]
     heap = MaxHeap.heapify(lst)
     assert not heap.is_empty()
-    assert heap.to_list() == [7, 2, 6, 1]
+    assert verify_max_heap(heap._transform().root)
     assert len(heap) == len(lst)
     assert heap.get_max() == max(lst)
     assert heap.get_min() == min(lst)
@@ -203,4 +206,53 @@ def test_heapify_of_max_heap_small_example():
     with pytest.raises(TypeError):
         get_list() in heap
         get_string() in heap
+
+
+def test_heapify_of_max_heap_big_example():
+    lst = [1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17, 90, 100, 102, 190]
+    heap = MaxHeap.heapify(lst)
+    assert verify_max_heap(heap._transform().root)
+    assert len(heap) == len(lst)
+    assert heap.get_max() == max(lst)
+    assert heap.get_min() == min(lst)
+    for node, value in zip(heap, [190, 15, 102, 8, 13, 17, 100, 1, 5, 4, 9, 3, 10, 6, 90]):
+        assert node.get_data() == value
+    for num in lst:
+        assert num in heap
+    with pytest.raises(TypeError):
+        get_list() in heap
+        get_string() in heap
+
+
+def test_insert_remove_max_heap():
+    heap = MaxHeap()
+    heap.insert(35)
+    heap.insert(33)
+    heap.insert(42)
+    heap.insert(10)
+    heap.insert(14)
+    heap.insert(19)
+    heap.insert(27)
+    heap.insert(44)
+    heap.insert(26)
+    heap.insert(31)
+    assert verify_max_heap(heap._transform().root)
+    assert len(heap) == 10
+    assert not heap.is_empty()
+    assert heap.get_min() == 10
+    assert heap.get_max() == 44
+    heap.remove(0) #does nothing
+    assert len(heap) == 10
+    heap.remove(10)
+    assert len(heap) == 9
+    assert heap.get_min() == 14
+    assert heap.get_max() == 44
+    assert verify_max_heap(heap._transform().root)
+    heap.remove(44)
+    assert len(heap) == 8
+    assert heap.get_min() == 14
+    assert heap.get_max() == 42
+    assert verify_max_heap(heap._transform().root)
+
+
 
