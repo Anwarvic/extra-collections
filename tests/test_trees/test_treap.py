660fae47d3869674193333d19b6cd71277ec8096
anwarvic <mohamedanwarvic@gmail.com>
2020-03-23 21:36:01 +0200

    added one more test-case for Treap

--- tests/test_trees/test_treap.py
+++ tests/test_trees/test_treap.py
@@ -25,3 +25,45 @@ def test_treap_node():
         assert node.get_children() == []
         with pytest.raises(AssertionError): node.set_priority(get_string())
         with pytest.raises(AssertionError): node.set_priority(get_list())
+
+
+def test_treap_one_value():
+    val = get_int()
+    treap = Treap(val)
+    # test structure
+    assert verify_bst_rules(treap.root)
+    assert verify_treap_priority(treap.root)
+    assert treap.root.get_data() == val
+    # test various methods
+    assert treap.count_leaf_nodes() == 1
+    assert treap.get_max() == val
+    assert treap.get_min() == val
+    assert treap.get_height() == 0
+    assert treap.is_balanced()
+    assert treap.is_perfect()
+    assert treap.is_strict()
+    # test __contains__
+    assert val in treap
+    assert 100 not in treap
+    assert 1.1 not in treap
+    # test __iter__ / traverse
+    assert treap.to_list() == [val]
+    assert treap.preorder_traverse() == [val]
+    assert treap.depth_first_traverse() == [val]
+    assert treap.postorder_traverse() == [val]
+    assert treap.inorder_traverse() == [val]
+    #test remove
+    treap.remove(9) #do nothing
+    with pytest.raises(IndexError): treap.remove(val)
+    # validate
+    with pytest.raises(TypeError): None in treap
+    with pytest.raises(TypeError): get_string() in treap
+    with pytest.raises(TypeError): get_list() in treap
+    with pytest.raises(TypeError): treap.insert(None)
+    with pytest.raises(TypeError): treap.insert(get_string())
+    with pytest.raises(TypeError): treap.insert(get_list())
+    with pytest.raises(TypeError): treap.insert(get_int(), get_string())
+    with pytest.raises(TypeError): treap.insert(get_int(), get_list())
+    with pytest.raises(TypeError): treap.remove(None)
+    with pytest.raises(TypeError): treap.remove(get_string())
+    with pytest.raises(TypeError): treap.remove(get_list())
