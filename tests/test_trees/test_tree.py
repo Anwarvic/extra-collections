bacfad6652a16aab0ca3f52d4ee34ad7107f2c74
anwarvic <mohamedanwarvic@gmail.com>
2020-04-11 00:18:50 +0200

    added one more test-case for Tree

--- tests/test_trees/test_tree.py
+++ tests/test_trees/test_tree.py
@@ -16,32 +16,42 @@ def test_treenode():
         assert node.get_children() == []
 
 
-# def test_tree_with_known_values():
-#     # create Simpsons Family tree
-#     root = TreeNode('TheSimpsons')
-#     # homer-side
-#     abraham = TreeNode('Abraham + Mona')
-#     herb = TreeNode('Herb')
-#     homer = TreeNode('Homer')
-#     abraham.children = [herb, homer]
-    
-#     # marge-side
-#     jackie = TreeNode('Clancy + Jackie')
-#     marge = TreeNode('Marge')
-#     patty = TreeNode('Patty')
-#     selma = TreeNode('Selma')
-#     ling = TreeNode('Ling')
-
-#     selma.children = [ling]
-#     jackie.children = [marge, patty, selma]
-
-#     # homer-marge children
-#     bart = TreeNode('Bart')
-#     lisa = TreeNode('Lisa')
-#     maggie = TreeNode('Maggie')
-#     homer.children = [bart, lisa, maggie]
-#     marge.children = homer.children
-#     # set root
-#     root.children = [abraham, jackie]
-#     t = Tree(root)
-#     print(t)
+def test_tree_with_known_values():
+    # create Simpsons Family tree
+    root = TreeNode('TheSimpsons')
+    # homer-side
+    abraham = TreeNode('Abraham + Mona')
+    herb = TreeNode('Herb')
+    homer = TreeNode('Homer')
+    abraham.children = [herb, homer]
+    # marge-side
+    jackie = TreeNode('Clancy + Jackie')
+    marge = TreeNode('Marge')
+    patty = TreeNode('Patty')
+    selma = TreeNode('Selma')
+    ling = TreeNode('Ling')
+    selma.children = [ling]
+    jackie.children = [marge, patty, selma]
+    # homer-marge children
+    bart = TreeNode('Bart')
+    lisa = TreeNode('Lisa')
+    maggie = TreeNode('Maggie')
+    homer.children = [bart, lisa, maggie]
+    marge.children = homer.children
+    # set root
+    root.children = [abraham, jackie]
+    t = Tree(root)
+    assert len(t) == 15
+    assert t.get_depth() == t.get_height() == 3
+    assert t.count_leaf_nodes() == 9
+    assert t.to_list() == \
+        ['TheSimpsons', 'Abraham + Mona', 'Clancy + Jackie', 'Herb', 'Homer',
+        'Marge', 'Patty', 'Selma', 'Bart', 'Lisa', 'Maggie', 'Bart', 'Lisa',
+        'Maggie', 'Ling']
+    assert t.get_nodes() == \
+        [
+            [TreeNode("TheSimpsons")],
+            [TreeNode("Abraham + Mona"), TreeNode("Clancy + Jackie")],
+            [TreeNode("Herb"), TreeNode("Homer"), TreeNode("Marge"), TreeNode("Patty"), TreeNode("Selma")], 
+            [TreeNode("Bart"), TreeNode("Lisa"), TreeNode("Maggie"), TreeNode("Bart"), TreeNode("Lisa"), TreeNode("Maggie"), TreeNode("Ling")]
+        ]
