ba0ee813e4e63bcc2a6bd4b2ec2dc6619dbab1bb
anwarvic <mohamedanwarvic@gmail.com>
2020-04-14 08:29:42 +0200

    moved _validate_item() to Extra object

--- extra/interface.py
+++ extra/interface.py
@@ -2,3 +2,12 @@ class Extra:
     
     def __name__(self):
         return "extra.Extra()"
+    
+
+    def _validate_item(self, item):
+        if item is None:
+            raise ValueError(\
+                f"Can't use `None` as an initial value for {self.__name__()}!!")
+        elif isinstance(item, Extra):
+            raise ValueError(\
+            f"Can't create {self.__name__()} object using {item.__name__()}!!")
