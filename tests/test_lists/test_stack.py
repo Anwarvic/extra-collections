cea56bdc7686aead40432f8a81b7abcdc720ac56
anwarvic <mohamedanwarvic@gmail.com>
2020-04-25 23:15:25 +0200

    changed test_stack to test my consistency rules over Stack

--- tests/test_lists/test_stack.py
+++ tests/test_lists/test_stack.py
@@ -7,19 +7,19 @@ from extra.lists.stack import Stack
 def test_creating_stack():
     # empty stack
     s = Stack()
-    assert s.max_capacity == float("inf")
-    assert s.container == []
+    assert s._max_capacity == float("inf")
+    assert s._container == []
     assert len(s) == 0
     assert s.is_empty()
     assert s.is_full() == False
     with pytest.warns(UserWarning): assert s.pop() == None
     with pytest.raises(IndexError): s.peek()
     s.clear() #not to through any errors
-    assert s.max_capacity == float("inf")
+    assert s._max_capacity == float("inf")
     # empty stack with max_capacity == 0
     s = Stack(max_capacity=0)
-    assert s.max_capacity == 0
-    assert s.container == []
+    assert s._max_capacity == 0
+    assert s._container == []
     assert len(s) == 0
     assert s.is_empty()
     assert s.is_full()
@@ -27,17 +27,17 @@ def test_creating_stack():
     with pytest.raises(IndexError): s.peek()
     with pytest.raises(OverflowError): s.push(get_value() )
     s.clear() #not to through any errors
-    assert s.max_capacity == 0
+    assert s._max_capacity == 0
 
 
 def test_stack_with_max_capacity():
     cap = get_pos_int()
     lst = get_list(length=cap)
     s = Stack(max_capacity= cap)
-    assert s.max_capacity == cap
+    assert s._max_capacity == cap
     for i in lst:
         s.push(i)
-    assert s.container == lst
+    assert s._container == lst
     assert len(s) == cap
     assert not s.is_empty()
     assert s.is_full()
@@ -51,7 +51,7 @@ def test_stack_with_max_capacity():
     s.push(get_value())
     # test max capacity after clear
     s.clear()
-    assert s.max_capacity == cap
+    assert s._max_capacity == cap
 
 
 def test_stack_with_invalid_max_capacity():
@@ -67,7 +67,7 @@ def test_creating_stack_with_random_numbers():
     s = Stack()
     for i in lst:
         s.push(i)
-    assert s.container == lst
+    assert s._container == lst
     assert len(s) == len(lst)
     assert s.peek() == lst[-1]
     assert not s.is_empty()
@@ -87,23 +87,23 @@ def test_stack_with_known_values():
     s.push(40)
     s.push(800)
     assert len(s) == 3
-    assert s.container == [2, 40, 800]
+    assert s._container == [2, 40, 800]
     with pytest.raises(OverflowError): s.push(16000)
     assert s.peek() == 800
     assert s.pop() == 800
-    assert s.container == [2, 40]
+    assert s._container == [2, 40]
     assert not s.is_empty()
     assert not s.is_full()
     s.clear()
     assert s.is_empty()
-    assert s.max_capacity == 3
+    assert s._max_capacity == 3
     assert s.is_full() == False
 
 
 def test_push_method():
     s = Stack()
-    with pytest.raises(TypeError): s.push(None)
-    with pytest.raises(ValueError): s.push('')
+    with pytest.raises(ValueError): s.push(None)
+    s.push('')
     s.push(get_value())
     s.push(get_int())
     s.push(get_string())
