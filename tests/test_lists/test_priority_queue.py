e9fdec45bef2fa99f88aaa677408c97c5fc22eb6
anwarvic <mohamedanwarvic@gmail.com>
2020-05-01 01:35:01 +0200

    adapted test_priority_queue.py script to my rules'

--- tests/test_lists/test_priority_queue.py
+++ tests/test_lists/test_priority_queue.py
@@ -8,28 +8,28 @@ from extra.lists.priority_queue import PriorityNode, PriorityQueue
 def test_creating_empty_priority_queue():
     # empty priority queue
     q = PriorityQueue()
-    assert q.max_capacity == float("inf")
-    assert q.min_priority == float("inf")
-    assert q.max_priority == float("-inf")
+    assert q._max_capacity == float("inf")
+    assert q._min_priority == float("inf")
+    assert q._max_priority == float("-inf")
     assert len(q) == 0
     assert q.is_empty()
     with pytest.warns(UserWarning): assert q.dequeue() == None
     with pytest.raises(IndexError): q.top()
     q.clear() #not to through any errors
-    assert q.max_capacity == float("inf")
+    assert q._max_capacity == float("inf")
     assert not q.is_full()
     # empty queue with max_capacity == 0
     q = PriorityQueue(max_capacity=0)
-    assert q.max_capacity == 0
-    assert q.min_priority == float("inf")
-    assert q.max_priority == float("-inf")
+    assert q._max_capacity == 0
+    assert q._min_priority == float("inf")
+    assert q._max_priority == float("-inf")
     assert len(q) == 0
     assert q.is_empty()
     with pytest.warns(UserWarning): assert q.dequeue() == None
     with pytest.raises(IndexError): q.top()
     with pytest.warns(UserWarning): q.enqueue(get_value() )
     q.clear() #not to through any errors
-    assert q.max_capacity == 0
+    assert q._max_capacity == 0
     assert q.is_full()
 
 
@@ -37,7 +37,7 @@ def test_queue_with_max_capacity():
     cap = get_pos_int()
     lst = get_list(length=cap)
     q = PriorityQueue(max_capacity=cap)
-    assert q.max_capacity == cap
+    assert q._max_capacity == cap
     for i in lst:
         q.enqueue(i)
     assert len(q) == cap
@@ -51,7 +51,7 @@ def test_queue_with_max_capacity():
     q.enqueue(get_value())
     # test max capacity after clear
     q.clear()
-    assert q.max_capacity == cap
+    assert q._max_capacity == cap
 
 
 def test_queue_with_invalid_max_capacity():
@@ -125,6 +125,6 @@ def test_queue_with_known_values():
     assert q.is_full()
     q.clear()
     assert q.is_empty()
-    assert q.max_capacity == 3
+    assert q._max_capacity == 3
     assert q.is_full() == False
 
