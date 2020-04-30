c5deec3c00a0cfdb36b00d7533ac4baaf9b389c5
anwarvic <mohamedanwarvic@gmail.com>
2020-05-01 00:45:58 +0200

    adapted test_queue.py script

--- tests/test_lists/test_queue.py
+++ tests/test_lists/test_queue.py
@@ -9,24 +9,24 @@ from extra.lists.queue import Queue
 def test_creating_empty_queue():
     # empty queue
     q = Queue()
-    assert q.max_capacity == float("inf")
+    assert q._max_capacity == float("inf")
     assert len(q) == 0
     assert q.is_empty()
     with pytest.warns(UserWarning): assert q.dequeue() == None
     with pytest.raises(IndexError): q.top()
     q.clear() #not to through any errors
-    assert q.max_capacity == float("inf")
+    assert q._max_capacity == float("inf")
     assert not q.is_full()
     # empty queue with max_capacity == 0
     q = Queue(max_capacity=0)
-    assert q.max_capacity == 0
+    assert q._max_capacity == 0
     assert len(q) == 0
     assert q.is_empty()
     with pytest.warns(UserWarning): assert q.dequeue() == None
     with pytest.raises(IndexError): q.top()
     with pytest.warns(UserWarning): q.enqueue(get_value() )
     q.clear() #not to through any errors
-    assert q.max_capacity == 0
+    assert q._max_capacity == 0
     assert q.is_full()
 
 
@@ -34,7 +34,7 @@ def test_queue_with_max_capacity():
     cap = get_pos_int()
     lst = get_list(length=cap)
     q = Queue(max_capacity=cap)
-    assert q.max_capacity == cap
+    assert q._max_capacity == cap
     for i in lst:
         q.enqueue(i)
     assert len(q) == cap
@@ -48,7 +48,7 @@ def test_queue_with_max_capacity():
     q.enqueue(get_value())
     # test max capacity after clear
     q.clear()
-    assert q.max_capacity == cap
+    assert q._max_capacity == cap
 
 
 def test_queue_with_invalid_max_capacity():
@@ -90,14 +90,14 @@ def test_queue_with_known_values():
     assert not q.is_full()
     q.clear()
     assert q.is_empty()
-    assert q.max_capacity == 3
+    assert q._max_capacity == 3
     assert q.is_full() == False
 
 
 def test_enqueue_method():
     q = Queue()
-    with pytest.raises(TypeError): q.enqueue(None)
-    with pytest.raises(ValueError): q.enqueue('')
+    with pytest.raises(ValueError): q.enqueue(None)
+    q.enqueue('')
     q.enqueue(get_value())
     q.enqueue(get_int())
     q.enqueue(get_string())
