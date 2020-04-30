4ed50af149fc061f2f15637042a32a312cf5a9f6
anwarvic <mohamedanwarvic@gmail.com>
2020-05-01 00:45:41 +0200

    applied my consistency rules over Queue class

--- extra/lists/queue.py
+++ extra/lists/queue.py
@@ -1,10 +1,11 @@
 import warnings
+from extra.interface import Extra
 from extra.lists.doubly_linked_list import DoublyLinkedList
 
 
 
 
-class Queue():
+class Queue(Extra):
     """Basic object for the Queue data structure"""
     def __name__(self):
         return "extra.Queue()"
@@ -12,23 +13,28 @@ class Queue():
     
     def __init__(self, max_capacity=float("inf")):
         if type(max_capacity) not in {int, float}:
-            raise TypeError("Max Capacity has to be a number!!")
+            raise TypeError(
+                f"Max Capacity of {self.__name__()} has to be a number!!")
         elif max_capacity < 0:
-            raise ValueError(f"Max capacity of {self.__name__()} must be >= 0!")
-        self.container = DoublyLinkedList()
-        self.max_capacity = max_capacity
+            raise ValueError(f"Max capacity of {self.__name__()} must be >= 0")
+        self._container = DoublyLinkedList()
+        self._max_capacity = max_capacity
 
 
-    ############################## PRINT ##############################
+    ##############################      PRINT     ##############################
     def _print_queue(self, direction_char=' '):
         top_border = '─┬'
         middle_border = direction_char+'│'
         down_border = '─┴'
-        for item in self.container:
-            width = len(item._represent())+2 #2: for a space before & after item
+        counter = 0
+        curr_node = self._container._head
+        while counter < len(self._container):
+            #NOTE: +2 for a space before & after `curr_node`
+            width = len(curr_node._represent())+2
             top_border += ('─'*width) + '┬'
-            middle_border += f" {item._represent()} │"
+            middle_border += f" {curr_node._represent()} │"
             down_border += ('─'*width) + '┴'
+            curr_node = curr_node.get_next()
         # add extension
         if not self.is_empty():
             top_border += '─'
@@ -42,67 +48,59 @@ class Queue():
         return self._print_queue(direction_char='⟶')
 
 
-    ############################## LENGTH ##############################
+    ##############################     LENGTH     ##############################
     def __len__(self):
         """Gets the length of the linked list with complexity of O(1)"""
-        return len(self.container)
+        return len(self._container)
 
 
-    ############################## GET ################################
+    ##############################      GET     ################################
     def top(self):
         """Returns the Qeueu head (first element to be inserted) """
         if self.is_empty():
             raise IndexError(\
                 f"Can't retrieve from an empty {self.__name__()}!!")
-        return self.container.tail.get_data()
+        return self._container._tail.get_data()
 
 
-    ############################# ENQUEUE ##############################
-    def _validate_item(self, item):
-        if item is None:
-            raise TypeError(f"Can't push `None` into {self.__name__()}!!")
-        elif type(item) == str and item.strip() == '':
-            raise ValueError(\
-                f"Can't push an empty-string into {self.__name__()}!!")
-    
-
+    ##############################     ENQUEUE    ##############################
     def _enqueue(self, item):
         assert item is not None
         if self.is_full():
             warnings.warn(f"Enqueuing to a full {self.__name__()} "+\
                 "could lead to missing values!!", UserWarning)
-            self.container.remove_end()
-        return self.container._insert(0, item)
+            self._container.remove_end()
+        return self._container._insert(0, item)
 
 
     def enqueue(self, item):
         """Insert value into the Queue"""
-        self._validate_item(item)
+        super()._validate_item(item)
         self._enqueue(item)
 
 
-    ##############################    DEQUEUE    ##############################
+    ##############################    DEQUEUE     ##############################
     def dequeue(self):
         """Removes value from the Queue (Queue's head)"""
         if self.is_empty():
             warnings.warn(f"Dequeuing from an empty {self.__name__()}!!")
-            return None
+            return
         else:
-            return self.container.remove_end().get_data()
+            return self._container.remove_end().get_data()
 
 
     def clear(self):
         """Clears the whole Queue"""
-        self.__init__(max_capacity=self.max_capacity)
+        self.__init__(max_capacity=self._max_capacity)
 
 
     ##############################     STATUS     ##############################
     def is_empty(self):
         """Checks if the Queue is empty"""
-        return self.container.is_empty()
+        return self._container.is_empty()
 
 
     def is_full(self):
-        return len(self) == self.max_capacity
+        return len(self) >= self._max_capacity
 
 
