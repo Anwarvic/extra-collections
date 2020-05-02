e920335d472510888f097dc921c95d6499eed386
anwarvic <mohamedanwarvic@gmail.com>
2020-05-02 03:59:07 +0200

    first attempt to apply my consistency rules over Circular Linked List

--- extra/lists/circular_linked_list.py
+++ extra/lists/circular_linked_list.py
@@ -11,17 +11,12 @@ class CircularLinkedList(LinkedList):
     def __name__(self):
         return "extra.CircularLinkedList()"
     
-
-    def __init__(self, item=None):
-        super().__init__(item)
-        self.head.next = self.head
-
-    
+   
     def _create_instance(self):
         return CircularLinkedList()
     
 
-    ############################## PRINT ##############################
+    ##############################      PRINT     ##############################
     def __repr__(self):
         """Represents the Circular linked list as a string
         ┌───┐ ┌───┐ ┌───┐ ┌───┐ ┌───┐ ┌───┐ ┌───┐ 
@@ -33,10 +28,10 @@ class CircularLinkedList(LinkedList):
         if self.is_empty():
             return super()._print_empty_linked_list()
         # start with just the head
-        first_line, second_line, third_line = self._print_node(self.head)
+        first_line, second_line, third_line = self._print_node(self._head)
         # print the remaining nodes if found
         top_part, middle_part, lower_part = \
-            self._print_linked_list(self.head.get_next())
+            self._print_linked_list(self._head.get_next())
         first_line  += top_part
         second_line += middle_part
         third_line  += lower_part
@@ -48,7 +43,7 @@ class CircularLinkedList(LinkedList):
         second_line = "".join(second_line)
         third_line  = "".join(third_line)
 
-        head_data = str(self.head.get_data())
+        head_data = str(self._head.get_data())
         left_offset = (len(head_data)+4)//2
         remaining = (len(second_line) - 2) - left_offset
         
@@ -58,26 +53,26 @@ class CircularLinkedList(LinkedList):
             first_line, second_line, third_line, fourth_line, fifth_line)
 
 
-    ############################## SEARCH ##############################
-    def __contains__(self, value):
-        #NOTE: DON'T validate the given value
-        # check if value is a Node() object
-        if isinstance(value, Node):
-            # if value is the same object as self._basic_node
-            if isinstance(value, self._basic_node):
-                value = value.get_data()
-            # if value is a generic Node object
-            else:
-                return False
-        # check the head
-        if self.head.get_data() == value:
-            return True
-        # search the rest of the CircularLinkedList()
-        found_node = self._search( value,
-                                start_node = self.head.get_next())
-        if found_node == None or found_node.get_data() != value:
-            return False
-        return True
+    ##############################     SEARCH     ##############################
+    # def __contains__(self, value):
+    #     #NOTE: DON'T validate the given value
+    #     # check if value is a Node() object
+    #     if isinstance(value, Node):
+    #         # if value is the same object as self._basic_node
+    #         if isinstance(value, self._basic_node):
+    #             value = value.get_data()
+    #         # if value is a generic Node object
+    #         else:
+    #             return False
+    #     # check the head
+    #     if self._head.get_data() == value:
+    #         return True
+    #     # search the rest of the CircularLinkedList()
+    #     found_node = self._search( value,
+    #                             start_node = self._head.get_next())
+    #     if found_node == None or found_node.get_data() != value:
+    #         return False
+    #     return True
 
 
     def _validate_index(self, idx, accept_negative=False, accept_slice=False):
@@ -97,68 +92,69 @@ class CircularLinkedList(LinkedList):
         self._validate_index(idx)
         if self.is_empty():
             raise IndexError(f"{self.__name__()} is empty!!")
-        idx = idx % self.length if self.length != 0 else 0
+        idx = idx % self._length if self._length != 0 else 0
         return super().__getitem__(idx)
 
 
-    ############################## INSERT ##############################
+    ##############################     INSERT     ##############################
     def _insert_node(self, prev_node, new_node):
         assert isinstance(new_node, self._basic_node)
         assert new_node.get_data() is not None
+
         # start inserting the node
-        if self.length == 0:
-            self.head.set_data(new_node.get_data())
-            new_node = self.head
-            self.head = new_node
+        if self._length == 0:
+            new_node.set_next(new_node)
+            self._head = new_node
         elif prev_node is None:
-            new_node.set_next(self.head.get_next())
-            self.head.set_next(new_node)
-            #swap data between new_node and self.head
-            new_node.data, self.head.data = self.head.data, new_node.data
-            new_node = self.head #to be returned
+            new_node.set_next(self._head.get_next())
+            self._head.set_next(new_node)
+            #swap data between new_node and self._head
+            new_node._data, self._head._data = self._head._data, new_node._data
+            new_node = self._head #to be returned
         else:
             new_node.set_next(prev_node.get_next())
             prev_node.set_next(new_node)
-        self.length += 1
+        self._length += 1
         return new_node
     
     
     def insert(self, idx, item):
         self._validate_index((idx))
         self._validate_item(item)
-        idx = idx % (self.length+1)
+        idx = idx % (self._length+1)
         super()._insert(idx, item)
 
 
-    ############################### SET ################################
+    ##############################       SET      ##############################
     def __setitem__(self, idx, item):
         self._validate_index(idx)
-        idx = idx % self.length if self.length != 0 else 0
+        idx = idx % self._length if self._length != 0 else 0
         super()._replace_node(idx, item)
     
 
-    ############################## REMOVE ##############################
+    ##############################     REMOVE     ##############################
     def _remove_node(self, prev_node, node_to_be_removed):
         assert prev_node is None or isinstance(prev_node, self._basic_node)
         assert isinstance(node_to_be_removed, self._basic_node)
+
         # if node to be removed is the first
         if prev_node is None:
-            if self.length == 1:
-                self.head.data = None #NOTE: don't use set_data() here
+            if self._length == 1:
+                self._head._data = None #NOTE: don't use set_data() here
             else:
-                next_to_head = self.head.get_next()
-                self.head.set_data(next_to_head.get_data())
-                self.head.set_next(next_to_head.get_next())
+                next_to_head = self._head.get_next()
+                self._head.set_data(next_to_head.get_data())
+                self._head.set_next(next_to_head.get_next())
         else:
             prev_node.set_next(node_to_be_removed.get_next())
-        self.length -= 1
+        self._length -= 1
     
 
     def __delitem__(self, idx):
         """Removes a node at index=idx from the Circular Linked List"""
         self._validate_index(idx)
         if not self.is_empty():
-            idx = idx % self.length if self.length != 0 else 0
+            idx = idx % self._length if self._length != 0 else 0
             node = super()._remove_idx(idx)
             return node
 
@@ -166,7 +162,7 @@ class CircularLinkedList(LinkedList):
     ############################## MISC ##############################
     def split(self, idx):
         self._validate_index(idx)
-        idx = idx % self.length if self.length != 0 else 0
+        idx = idx % self._length if self._length != 0 else 0
         return super()._split(idx)
     
 
