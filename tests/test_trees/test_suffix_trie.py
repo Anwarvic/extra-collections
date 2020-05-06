dba61188bcc4969f299f4b7081cfebcb1e08587c
anwarvic <mohamedanwarvic@gmail.com>
2020-05-06 02:48:23 +0200

    added first test function to Suffix Trie

--- tests/test_trees/test_suffix_trie.py
+++ tests/test_trees/test_suffix_trie.py
@@ -4,4 +4,26 @@ from extra.trees.suffix_trie import SuffixTrie
 
 
 
+def test_lcs():
+    st = SuffixTrie("ABABABA")
+    assert st.get_lcs() == ["ABABA"]
+
+    st = SuffixTrie("banana")
+    assert st.get_lcs() == ["ana"]
+
+    st = SuffixTrie("GEEKSFORGEEKS")
+    assert st.get_lcs() == ["GEEKS"]
+
+    st = SuffixTrie("AAAAAAAAAA")
+    assert st.get_lcs() == ["AAAAAAAAA"]
+
+    st = SuffixTrie("ABCDEFG")
+    assert st.get_lcs() == [""]
+
+    st = SuffixTrie("ATCGATCGA")
+    assert st.get_lcs() == ["ATCGA"]
+
+    st = SuffixTrie("abcpqrabpqpq")
+    assert sorted(st.get_lcs()) == ["ab", "pq"]
+
     
