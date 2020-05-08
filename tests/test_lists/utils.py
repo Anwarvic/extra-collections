727720447cdbb2b061bdfec8fdb9f7d4994e0a9d
anwarvic <mohamedanwarvic@gmail.com>
2020-05-08 03:46:48 +0200

    adding more helper functions in utils.py script

--- tests/test_lists/utils.py
+++ tests/test_lists/utils.py
@@ -1,5 +1,8 @@
 import random
 
+
+
+
 def get_int(a=-1000, b=1000):
     return random.randint(a, b)
 
@@ -14,3 +17,16 @@ def get_neg_int(a=-1000, b=0):
 
 def get_float(a=-1000, b=1000):
     return random.uniform(a, b)
+
+
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+def get_string(length=5):
+    s = []
+    for _ in range(length):
+        s += ALPHABET[get_pos_int(b=length)]
+    return "".join(s)
+
+
+def get_value():
+    func = random.choice([get_int, get_float, get_string])
+    return func()
