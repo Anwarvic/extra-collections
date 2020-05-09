791bca3cc81291dc42db5c1e7b018a0baa5ecb56
anwarvic <mohamedanwarvic@gmail.com>
2020-05-10 01:21:43 +0200

    added testig for count_pattern_occurrence() method of Suffix Trie

--- tests/test_trees/test_suffix_trie.py
+++ tests/test_trees/test_suffix_trie.py
@@ -11,8 +11,8 @@ def test_lcs():
     st = SuffixTrie("banana")
     assert st.get_lcs() == ["ana"]
 
-    st = SuffixTrie("GEEKSFORGEEKS")
-    assert st.get_lcs() == ["GEEKS"]
+    st = SuffixTrie("PAPERSFORPAPERS")
+    assert st.get_lcs() == ["PAPERS"]
 
     st = SuffixTrie("AAAAAAAAAA")
     assert st.get_lcs() == ["AAAAAAAAA"]
@@ -26,4 +26,19 @@ def test_lcs():
     st = SuffixTrie("abcpqrabpqpq")
     assert sorted(st.get_lcs()) == ["ab", "pq"]
 
+
+def test_count_pattern_occurrence():
+    # in case of: banana
+    st = SuffixTrie("banana")
+    assert st.count_pattern_occurrences("bi") == 0
+    assert st.count_pattern_occurrences("b") == 1
+    assert st.count_pattern_occurrences("a") == 3
+    assert st.count_pattern_occurrences("an") == 2
+    assert st.count_pattern_occurrences("ana") == 2
+    assert st.count_pattern_occurrences("anana") == 1
+    assert st.count_pattern_occurrences("banana") == 1
+    assert st.count_pattern_occurrences("ba") == 1
+    assert st.count_pattern_occurrences("ban") == 1
+    assert st.count_pattern_occurrences("bann") == 0
+    assert st.count_pattern_occurrences("bananab") == 0
     
