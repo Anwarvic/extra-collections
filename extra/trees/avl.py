e4d1ba8b6e8a3344e67791b838c6f26716b8cde4
anwarvic <mohamedanwarvic@gmail.com>
2020-05-15 23:12:28 +0200

    added __name__() method to AVLNode

--- extra/trees/avl.py
+++ extra/trees/avl.py
@@ -2,7 +2,9 @@ from extra.trees.bst import BSTNode, BST
 
 
 
-class AVLnode(BSTNode):
+class AVLNode(BSTNode):
+    def __name__(self):
+        return "extra.AVLNode()"
     def __init__(self, value):
         super().__init__(value)
         self._height = 0
@@ -15,6 +17,13 @@ class AVLnode(BSTNode):
     def increment_height(self):
         self._height += 1
     
-    
+
     def decrement_height(self):
         self._height -= 1
+
+
+
+
+def AVL(BST):
+
+    def 
