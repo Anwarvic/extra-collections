ccb8e58b73ad90a036eb1c7fe5892e0aa87d2f67
anwarvic <mohamedanwarvic@gmail.com>
2020-05-15 23:29:47 +0200

    started with insert() method in AVL

--- extra/trees/avl.py
+++ extra/trees/avl.py
@@ -5,6 +5,8 @@ from extra.trees.bst import BSTNode, BST
 class AVLNode(BSTNode):
     def __name__(self):
         return "extra.AVLNode()"
+    
+
     def __init__(self, value):
         super().__init__(value)
         self._height = 0
@@ -25,5 +27,28 @@ class AVLNode(BSTNode):
 
 
 def AVL(BST):
+    _basic_node = AVLNode
 
-    def 
+
+    def __name__(self):
+        return "extra.AVL()"
+
+    
+    def insert(self, value):
+        self._validate_item(value)
+        if self.is_empty():
+            self._root = self._basic_node(value)
+            self._length += 1
+        else:
+            inserted_node = self._insert(value)
+            parent = inserted_node.get_parent()
+            while(parent is not None):
+                parent.increment_height()
+                parent = parent.get_parent()
+
+
+
+if __name__ == "__main__":
+    avl = AVL()
+    avl.insert(43)
+    print(avl)
