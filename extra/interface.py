268469c327e75d0e0737f205004a3e7e90b57c6b
anwarvic <mohamedanwarvic@gmail.com>
2020-05-24 00:53:21 +0200

    added the first page in the documentation

--- extra/interface.py
+++ extra/interface.py
@@ -1,10 +1,36 @@
+"""
+Extra Collections
+==================
+"extra-collections" ("extra" for short) is a python library that provides an
+intuitive pythonic implementation of the most common data structures used in 
+in most of the software projects in the market. Some of these data structures
+are simple such as `Stack` or `Queue`; and some are much more complicated such
+as `SkipList` or `RedBlackTree`.
+
+The name of this pacakge was inspired by a built-in python library called
+`collections <https://docs.python.org/3.8/library/collections.html>`_
+which provides a pythonic implementation for some basic data structures. "Extra"
+provides an extra group of data structures, hence the name "extra-collections".
+"""
+
 class Extra:
+    """An example docstring for a class definition."""
     
     def __name__(self):
+        """
+        Blah blah blah.
+        Parameters
+        ---------
+        name
+            A string to assign to the `name` instance attribute.
+        """
         return "extra.Extra()"
     
 
     def _validate_item(self, item):
+        """
+        Return information about an instance created from ExampleClass.
+        """
         if item is None:
             raise ValueError(\
                 f"Can't use `None` as an initial value for {self.__name__()}!!")
