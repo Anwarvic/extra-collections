6ecd8b0c0f8486cdf020708d1704385e3c81ed2b
anwarvic <mohamedanwarvic@gmail.com>
2020-05-24 16:04:57 +0200

    adding more details to the welcome page of the package documentation

--- extra/interface.py
+++ extra/interface.py
@@ -1,16 +1,47 @@
 """
 Extra Collections
 ==================
+
 "extra-collections" ("extra" for short) is a python library that provides an
 intuitive pythonic implementation of the most common data structures used in 
-in most of the software projects in the market. Some of these data structures
-are simple such as `Stack` or `Queue`; and some are much more complicated such
-as `SkipList` or `RedBlackTree`.
+most of the software projects in the market. Some of these data structures are
+simple such as `Stack` or `Queue`; and some are much more complicated such as
+`SkipList` or `RedBlackTree`.
 
-The name of this pacakge was inspired by a built-in python library called
-`collections <https://docs.python.org/3.8/library/collections.html>`_
-which provides a pythonic implementation for some basic data structures. "Extra"
-provides an extra group of data structures, hence the name "extra-collections".
+The name of this pacakge was inspired by the 
+`collections <https://docs.python.org/3.8/library/collections.html>`_ python
+library which provides a pythonic implementation for some basic data structures.
+"extra" provides an additional set of data structures, hence the name...
+"extra-collections".
+
+
+Available Data Structures
+-------------------------
+In this release, you can find +20 data structures that can be categorized into
+two categories:
+
+# List-based Data Structures:
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* Linked List
+* Doubly Linked List
+* Circular Linked List
+* Stack
+* Queue
+* PriorityQueue
+* Deque
+* SkipList
+
+# Tree-based Data Structures:
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+How to use the documentation
+----------------------------
+Documentation is available in two forms: docstrings provided
+with the code, and a loose standing reference guide, available from
+`the NumPy homepage <https://www.scipy.org>`_.
+
 """
 
 class Extra:
