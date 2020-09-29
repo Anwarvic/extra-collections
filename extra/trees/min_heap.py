c0bec1193d61b30626008de4c7e3ed88a046e168
anwar <mohamedanwarvic@gmail.com>
2020-09-29 04:29:58 +0200

    adding min heap gif to the docstring

--- extra/trees/min_heap.py
+++ extra/trees/min_heap.py
@@ -2,8 +2,8 @@
 A heap is a binary tree that stores a collection of nodes. Each node must
 satisfy two additional properties:
 
-1. Heap-Order Property: It is a relational property defined in terms of the 
-value stored at the node. So, for every node other than the root, the value
+1. **Heap-Order Property**: It is a relational property defined in terms of the 
+value stored at the node. So, for every node, other than the root, the value
 stored at the node is greater than or equal to the value stored at the node's
 parent. As a consequence of the heap-order property, the value encountered on a
 path from the root to a leaf node are in nondecreasing order. Also, the minimum
@@ -11,10 +11,12 @@ value is always stored at the root of the `MinHeap()`. This makes it easy to
 locate such a node when `get_min()` or `remove_min()` is called, as it is
 informally said to be at the top of the heap. Hence, the name "Min Heap"
 
-2. Perfect Binary Tree Property: It is a structural property defined in terms of
-the shape of heap itself. A binary tree is perfect if all its levels are
-completely filled. So, given an **n** inserted items, the height of the heap 
-should be **log(n)** at most.
+2. **Perfect Binary Tree Property**: It is a structural property defined in
+terms of the shape of heap itself. A binary tree is perfect if all its levels
+are completely filled. So, given an **n** inserted items, the height of the
+heap should be **log(n)** at most.
+
+.. image:: ../../img/trees/min_heap.gif
 
 The following table sums up all the different public functionality in this
 class and also provides the worst-case time complexity along side with the
@@ -68,8 +70,8 @@ from extra.trees._heap import Heap
 class MinHeap(Heap):
     """
     A Min heap is a perfect binary tree that stores a collection of nodes. Each
-    node other than stores a value greater than or equal to the value strored at
-    the node's parent.
+    node stores a value greater than or equal to the value strored at the node's
+    parent.
     """
     __name__ = "extra.MinHeap()"
     
