43b35c86234a4d55f569a3350150cfe744c72994
anwarvic <mohamedanwarvic@gmail.com>
2020-09-30 20:52:41 +0200

    created a simple treap object

--- Trees/treap.py
+++ Trees/treap.py
@@ -1,18 +1,26 @@
-from random import randint
+from random import seed, randint
 from bst import TreeNode, BST
 
-
-class TreeNode(TreeNode):
-	def __init__(self, key):
+seed(0)
+class TreapNode(TreeNode):
+    def __init__(self, key):
         super().__init__(key)
-        self.priority = randint(-1000, 1000)
+        self.priority = randint(0, 1000)
+
+    def represent_data(self):
+        return "{}|{}".format(self.data, self.priority)
 
 
 
 class Treap(BST):
-	pass
+    def __init__(self, key):
+        if isinstance(key, TreapNode):
+            self.root = key
+        else:
+            self.root = TreapNode(key)
 
 
 
 if __name__ == "__main__":
     t = Treap(1)
+    print(t)
