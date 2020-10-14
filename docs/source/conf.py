44ec4dbece738609cc6f03bbb5823a1995cf9e04
anwar <mohamedanwarvic@gmail.com>
2020-10-14 18:01:48 +0200

    adding 'sphinx.ext.todo' to sphinx configurations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,11 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage',
-				'sphinx.ext.napoleon']
+extensions = [	'sphinx.ext.autodoc',
+				'sphinx.ext.coverage',
+				'sphinx.ext.todo',
+				'sphinx.ext.napoleon'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
