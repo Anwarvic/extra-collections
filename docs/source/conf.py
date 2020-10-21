a409b7b3656d7e23f544d0c882e5767e0edb0dde
anwarvic <mohamedanwarvic@gmail.com>
2020-10-21 15:47:51 +0200

    updated conf.py to import extra

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,16 +10,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'extra-collections'
 copyright = '2020, anwarvic'
-author = 'anwarvic'
+author = 'Anwarvic'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
