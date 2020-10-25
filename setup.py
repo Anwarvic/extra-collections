1414db2172b693f163df33a22ea97037202b252f
anwarvic <mohamedanwarvic@gmail.com>
2020-10-25 23:57:13 +0200

    updated setup.py to release 1.0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GPL-3.0 License",
         "Operating System :: OS Independent",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries",
